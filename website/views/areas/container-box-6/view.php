e862e76a64bee205889c0c8430f2c49f9eab180d
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2014-08-08 15:33:06 +0700

    update

---
+++ website/views/areas/container-box-6/view.php
@@ -0,0 +1,142 @@
+<div class="items-container margin-image">
+	<div class="item" id="item-left">
+		<div class="heading">
+			<h2 class="title_news"><?php echo $this->link('title-1')?></h2>
+			<div class="btn-group">
+				<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+				<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<?php if($this->editmode) { ?>
+			<?php echo $this->image('image-1', array(
+					'class'  	=> 'image_share',
+					'thumbnail'	=> 'product')
+			);?>
+		<?php } else { ?>
+			<?php echo $this->image('image-1', array('image_share'))?>
+		<?php } ?>
+		<!-- <img class="image_share" src="_uploads/tentang-kami-profil-allianz.jpg" alt="unggul2 allianz tentang kami"> -->
+		<!-- <ul class="square-blue">
+			<li class="title">Produk sesuai tujuan:</li>
+			<li>Asuransi Mobil</li>
+			<li>Asuransi Kartu Proteksiku</li>
+			<li>Asuransi Dana Kesehatanku</li>
+			<li>Asuransi Rumahku Plus</li>
+		</ul> -->
+		<div class="description">
+			<p class="des"><?php echo $this->wysiwyg('description-1')?></p>
+		</div>
+	</div>
+
+	<div class="item" id="item-left">
+		<div class="heading">
+			<h2 class="title_news"><?php echo $this->link('title-2')?></h2>
+			<div class="btn-group">
+				<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+				<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<?php if($this->editmode) { ?>
+			<?php echo $this->image('image-2', array(
+					'class'  	=> 'image_share',
+					'thumbnail'	=> 'product')
+			);?>
+		<?php } else { ?>
+			<?php echo $this->image('image-2', array('image_share'))?>
+		<?php } ?>
+		<!-- 	<ul class="square-green">
+				<li class="title"></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>  -->
+		<div class="description">
+			<p class="des"><?php echo $this->wysiwyg('description-2')?></p>
+		</div>
+	</div>
+
+	<div class="item" id="item-right">
+		<div class="heading">
+			<h2 class="title_news"><?php echo $this->link('title-3')?></h2>
+			<div class="btn-group">
+				<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+				<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<?php if($this->editmode) { ?>
+			<?php echo $this->image('image-3', array(
+					'class'  	=> 'image_share',
+					'thumbnail'	=> 'product')
+			);?>
+		<?php } else { ?>
+			<?php echo $this->image('image-3', array('image_share'))?>
+		<?php } ?>
+		<div class="description">
+			<p class="des"><?php echo $this->wysiwyg('description-3')?></p>
+		</div>
+	</div>
+
+	<div class="item" id="item-left">
+		<div class="heading">
+			<h2 class="title_news"><?php echo $this->link('title-4')?></h2>
+			<div class="btn-group">
+				<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+				<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<?php if($this->editmode) { ?>
+			<?php echo $this->image('image-4', array(
+					'class'  	=> 'image_share',
+					'thumbnail'	=> 'product')
+			);?>
+		<?php } else { ?>
+			<?php echo $this->image('image-4', array('image_share'))?>
+		<?php } ?>
+		<div class="description">
+			<p class="des"><?php echo $this->wysiwyg('description-4')?></p>
+		</div>
+	</div>
+	
+	<div class="item" id="item-left">
+		<div class="heading">
+			<h2 class="title_news"><?php echo $this->link('title-5')?></h2>
+			<div class="btn-group">
+				<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+				<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<?php if($this->editmode) { ?>
+			<?php echo $this->image('image-5', array(
+					'class'  	=> 'image_share',
+					'thumbnail'	=> 'product')
+			);?>
+		<?php } else { ?>
+			<?php echo $this->image('image-5', array('image_share'))?>
+		<?php } ?>
+		<div class="description">
+			<p class="des"><?php echo $this->wysiwyg('description-5')?></p>
+		</div>
+	</div>
+	<!--
+	<div class="item" id="item-right">
+		<div class="heading">
+			<h2 class="title_news"><?php echo $this->link('title-6')?></h2>
+			<div class="btn-group">
+				<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+				<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+			</div>
+		</div>
+		<?php if($this->editmode) { ?>
+			<?php echo $this->image('image-6', array(
+					'class'  	=> 'image_share',
+					'thumbnail'	=> 'product')
+			);?>
+		<?php } else { ?>
+			<?php echo $this->image('image-6', array('image_share'))?>
+		<?php } ?>
+		<div class="description">
+			<p class="des"><?php echo $this->wysiwyg('description-6')?></p>
+		</div>
+	</div>
+	-->
+</div> <!-- .items-container -->
