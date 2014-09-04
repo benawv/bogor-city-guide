6c11990dc892392ba211f636dc866d0b3bc167e6
Robbi Nugraha <mrobbi.nugraha@gmail.com>
2014-09-04 09:54:36 +0700

    progress allianz community tips

---
+++ website/lib/Website/Model/vote.php
@@ -0,0 +1,69 @@
+<?php
+/* class Website_Model_Vote extends Pimcore_Model_Abstract {
+ 
+    public $id;
+    public $username;
+    public $score;
+    public $ip;
+    public $idA;
+ 
+    public function save() {
+        return $this->getResource()->save();
+    }
+ 
+    public static function getById($id) {
+        $obj = new self;
+        $obj->getResource()->getById($id);
+        return $obj;
+    }
+    
+    public static function getByIp($ip) {
+    	$obj = new self;
+    	$obj->getResource()->getByIp($ip);
+    	return $obj;
+    }
+    
+ 
+ 
+    // just getters/setters
+    public function setScore($score) {
+        $this->score = $score;
+    }
+ 
+    public function getScore() {
+        return $this->score;
+    }
+ 
+    public function setUsername($username) {
+        $this->username = $username;
+    }
+ 
+    public function getUsername() {
+        return $this->username;
+    }
+ 
+    public function setId($id) {
+        $this->id = $id;
+    }
+ 
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function setIpAddress($ip) {
+    	$this->ipAddress = $ip;
+    }
+    
+    public function getIpAddress() {
+    	return $this->ipAddress;
+    }
+    
+    public function setIdArticle($idA) {
+    	$this->idArticle = $idA;
+    }
+    
+    public function getIdArticle() {
+    	return $this->idArticle;
+    }
+
+} */
