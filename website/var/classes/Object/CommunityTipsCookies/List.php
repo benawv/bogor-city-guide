d77e6fef638ab2fb766783a00db851b22c119aba
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2014-09-10 10:49:11 +0700

    fixing .gitignore

--- website/var/classes/Object/CommunityTipsCookies/List.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-class Object_CommunityTipsCookies_List extends Object_List_Concrete {
-
-public $classId = 14;
-public $className = "communityTipsCookies";
-
-
-}
--- website/var/classes/Object/Fieldcollection/Data/Text.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-class Object_Fieldcollection_Data_Text extends Object_Fieldcollection_Data_Abstract  {
-
-public $type = "text";
-
-
-}
-
