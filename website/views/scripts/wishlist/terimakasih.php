c3e54e94de5d39575c3ddbf07cefc1f945799a6a
Robbi Nugraha <mrobbi.nugraha@gmail.com>
2014-11-11 12:36:04 +0700

    halaman terima kasih

--- website/views/scripts/wishlist/terimakasih.php
+++ website/views/scripts/wishlist/terimakasih.php
@@ -1,7 +1,14 @@
-<h1>Terima Kasih</h1>
-<p>
-	Pesanan Anda telah kami terima.
-</p>
-<p>
-	Silahkan cek email Anda untuk melihat rincian produk yang Anda pesan.
-</p>
+<div class="divTerima container boxes-view">
+	<div class="terimaKasih">
+		<div class="kata_kata">
+			<div class="kata-kiri">
+				<img src="/website/static/images/terima_kasih/logo.png" />
+			</div>
+			<div class="kata-kanan">
+				<h2>Terima Kasih</h2>
+				<h3>Pesan anda telah kami terima</h3>
+				Silahkan cek email Anda untuk melihat rincian produk yang Anda pesan.
+			</div>
+		</div>
+	</div>
+</div>
