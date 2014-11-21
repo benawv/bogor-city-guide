9aae694ace474c4b07df8327d06a50d8542d4989
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2014-11-21 09:21:24 +0700

    Update metadata.php
    add google analytics code

--- website/views/scripts/includes/metadata.php
+++ website/views/scripts/includes/metadata.php
@@ -60,6 +60,18 @@ if(!$this->editmode){
 <script type="text/javascript" src="/website/static/plugins/gmaps-marker-label.js"></script>
 
 <meta name="google-site-verification" content="t5S9yqQc-NyyfANDoR4qXDmEbpy746PnIqxU5rp0aXM" />
+
+<!--Google Analytics-->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-44171460-1', 'auto');
+ga('send', 'pageview');
+</script>
+<!--Google Analytics-->
+
 <script type="text/javascript">
 	$(function(){
 		$('#slideshow').flexslider({
