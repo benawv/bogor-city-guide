98ec27f73b82f4ec29503bb3e69e33a656a27f13
akbarfachri <akbar.fachriansyah@gmail.com>
2014-11-28 14:48:06 +0700

    email blast page

---
+++ website/static/virgin/email-blast.php
@@ -0,0 +1,72 @@
+<?php include("include/htaccess.php");?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<title>Allianz Indonesia : Asuransi Indonesia Terbaik</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+
+
+<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="blast.css">
+
+<meta name="author" content="Allianz">
+<meta name="description" content="Asuransi Allianz Indonesia adalah perusahaan asuransi terbaik dan terpercaya di dunia.">
+<meta name="keywords" content="Asuransi Indonesia, Allianz Indonesia, Asuransi Terbaik, Asuransi Terpercaya">
+<?php include("include/header.php"); ?>
+
+</head>
+
+<body>
+    
+
+
+<div role="main" class="main no-gutter">
+	<div class="blast-container">
+		
+		<!--jurnal -->
+		
+		<div id="community no-gutter no-margin" class="clearfix">
+			
+			<div class="blast-img">
+                   <img src="_assets/css/bootstrap/edwin.jpg" />
+			</div>
+            
+		</div> <!--Community-->
+		
+		<div id="community" class="clearfix padding no-margin">
+			
+            <div class="blast-title">
+                <h2>Allianz peduli dengan hal terpenting Anda</h2>
+            </div>
+            
+			<div class="blast-desc">
+				<p>Nikmati kemudahan akses untuk mengetahui status polis Anda melalui layanan digital customer portal dan aplikasi klaim kami.</p>
+			</div>
+            
+            <div class="blast-link">
+				<p>
+                    <span>Ayo ceritakan 1 yang terpenting di:</span>
+                    <span><a href="https://indonesia.allianz.com">https://indonesia.allianz.com</a></span>
+                </p>    
+			</div>
+            
+		</div> <!--Community-->
+        
+        <div id="community" class="clearfix blast-footer">
+			
+           
+		</div> <!--Community-->
+        
+        <div class="blast-weblogo">
+            "Allianz"
+            <span onclick="window.location.href = '/'"></span>
+        </div>
+		
+		
+	</div>
+</div>
+
+</html>
