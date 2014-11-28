c0bc0b8b6c7de8d1a91aef6840ebb4aca49f1a46
Robbi Nugraha <mrobbi.nugraha@gmail.com>
2014-11-28 17:12:36 +0700

    test send mail with template

--- website/views/scripts/email/emailwishlist.php
+++ website/views/scripts/email/emailwishlist.php
@@ -1,11 +1,81 @@
-<html>
-	<head>
-	</head>
-	<body>
-			<?php echo $this->image('icon', array(
+<?php include("include/htaccess.php");?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html lang="en">
+<head>
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<title>Allianz Indonesia : Asuransi Indonesia Terbaik</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<meta name="apple-mobile-web-app-capable" content="yes">
+
+
+<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/blast.css">
+
+<meta name="author" content="Allianz">
+<meta name="description" content="Asuransi Allianz Indonesia adalah perusahaan asuransi terbaik dan terpercaya di dunia.">
+<meta name="keywords" content="Asuransi Indonesia, Allianz Indonesia, Asuransi Terbaik, Asuransi Terpercaya">
+<?php include("include/header.php"); ?>
+
+</head>
+
+<body>
+    
+
+
+<div role="main" class="main no-gutter">
+	<div class="blast-container">
+		
+		<!--jurnal -->
+		
+		<div id="community no-gutter no-margin" class="clearfix">
+			
+			<div class="blast-img">
+                   <!-- <img src="_assets/css/bootstrap/edwin.jpg" /> -->
+                   <?php echo $this->image('imgEmail', array(
 									'title' 	=> 'Drag your icon here',
-									'width' 	=> 150,
-									'height' 	=> 150)) ?>
+									'width' 	=> 600,
+									'height' 	=> 400)) ?>
+			</div>
+            
+		</div> <!--Community-->
+		
+		<div id="community" class="clearfix padding no-margin">
+			
+            <div class="blast-title">
+                <h2>Allianz peduli dengan hal terpenting Anda</h2>
+            </div>
+            
+			<div class="blast-desc">
+				<p>Nikmati kemudahan akses untuk mengetahui status polis Anda melalui layanan digital customer portal dan aplikasi klaim kami.</p>
+			</div>
+            
+            <div class="blast-link">
+				<p>
+                    <span>Ayo ceritakan 1 yang terpenting di:</span>
+                    <span><a href="https://indonesia.allianz.com">https://indonesia.allianz.com</a></span>
+                </p>    
+			</div>
+            
+		</div> <!--Community-->
+        
+        <div id="community" class="clearfix blast-footer">
+			
+           
+		</div> <!--Community-->
+        
+        <div class="blast-weblogo">
+            "Allianz"
+            <span onclick="window.location.href = '/'"></span>
+        </div>
+		<?php echo $this->image('icon', array(
+									'title' 	=> 'Drag your icon here',
+									'width' 	=> 600,
+									'height' 	=> 415)) ?>
+			HELOOOOOOOO %Text(firstName); %Text(lastName);
 			<?php echo $this->wysiwyg('description')?>
-	</body>
+		
+	</div>
+</div>
+
 </html>
