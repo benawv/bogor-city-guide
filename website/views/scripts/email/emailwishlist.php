4fd5ba2c79e6311508ed51f4fa94c62857c43531
Robbi Nugraha <mrobbi.nugraha@gmail.com>
2014-12-01 13:06:12 +0700

    test send mail with template

--- website/views/scripts/email/emailwishlist.php
+++ website/views/scripts/email/emailwishlist.php
@@ -2,19 +2,17 @@
 <head>
 <!-- Meta Tags -->
 <meta charset="utf-8">
-<style type="text/css">
-	body{background: aqua;}
-</style>
 </head>
-<body>
-<div role="main" class="main no-gutter">
-	<div class="blast-container">
+<body style="font-size: 100%;background: none repeat scroll 0 0 #f5f6f6;color: #111;
+    font-family: Arial,Helvetica,sans-serif;text-align: center;">
+<div role="main" class="main no-gutter" style="clear: both;padding: 20px 0 0 10px;width: 100%;padding: 0 !important;">
+	<div class="blast-container" style="margin: 0 auto;padding: 0 15px;position: relative;text-align: left;width: 600px;">
 		
 		<!--jurnal -->
 		
 		<div id="community no-gutter no-margin" class="clearfix">
 			
-			<div class="blast-img">
+			<div class="blast-img" style="height: auto;">
                    <!-- <img src="_assets/css/bootstrap/edwin.jpg" /> -->
                    <?php echo $this->image('imgEmail', array(
 									'title' 	=> 'Drag your icon here',
@@ -24,20 +22,20 @@
             
 		</div> <!--Community-->
 		
-		<div id="community" class="clearfix padding no-margin">
+		<div id="community" class="clearfix padding no-margin" style="background: none repeat scroll 0 0 #fff;clear: both;margin-bottom: 20px;padding: 15px;padding: 15px 34px !important;margin: 0 !important;">
 			
             <?php echo $this->wysiwyg('description')?>
             
 		</div> <!--Community-->
         
-        <div id="community" class="clearfix blast-footer">
+        <div id="community" class="clearfix blast-footer" style="background: none repeat scroll 0 0 #fff;clear: both;margin-bottom: 20px;padding: 15px;background: none repeat scroll 0 0 #eee !important;height: 110px;margin: 0 !important;">
 			
            
 		</div> <!--Community-->
         
-        <div class="blast-weblogo">
+        <div class="blast-weblogo" style="background: none repeat scroll 0 0 #003781; bottom: 70px; display: block; height: 80px; margin: 0; overflow: hidden; padding: 0; position: absolute; right: 51px; text-indent: -9999px; width: 180px; z-index: 9999;">
             "Allianz"
-            <span onclick="window.location.href = '/'"></span>
+            <span style='background: url("/website/static/images/weblogo.png") no-repeat scroll center center / 78% auto rgba(0, 0, 0, 0); cursor: pointer; display: block; height: 100%; left: 0; position: absolute; top: 0; width: 100%;' onclick="window.location.href = '/'"></span>
         </div>
 			
 		
