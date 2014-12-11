f2f07fc217b4fb7845adbd997dc4e238c8bba89f
Robbi Nugraha <mrobbi.nugraha@gmail.com>
2014-12-11 13:33:22 +0700

    judul aman di jalan

--- website/views/scripts/content-layouts/categories2.php
+++ website/views/scripts/content-layouts/categories2.php
@@ -8,7 +8,7 @@
 <div class="container boxes-view">
 	<div class="heading clearfix">
 		<h5 style="visibility:hidden;"><span><a href="product.php">Produk</a></span> &raquo; <span><b>Asuransi Umum</b></span></h5>
-		<span class="title"><?php echo $this->document->getTitle()?><span>
+		<span class="title"><?php echo $this->document->getProperty('navigation_title');?><span>
 		<!--<div class="hotline"><img width="30px" src="_assets/images/hotline.png">&nbsp;&nbsp;<span style="font-size:20px"><strong>021-2926 9999</strong></span></div>-->
 	</div>
 	<div class="wrapper-special clearfix">
