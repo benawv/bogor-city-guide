81816fc991232aeebd5ff201e7861b80f0d784f1
Robbi Nugraha <mrobbi.nugraha@gmail.com>
2014-12-12 10:11:19 +0700

    test breadcrumb beta

--- website/views/scripts/includes/inv/header.php
+++ website/views/scripts/includes/inv/header.php
@@ -184,33 +184,4 @@
 	</div>
 	
 </header>
-<?php
-	if(!$this->editmode) { 
-		$url = $_SERVER['REQUEST_URI'];
-		$uri = explode("/", $url);
-		
-		for($x = 1; $x < count($uri); $x++)
-		{
-			$title = explode("-", $uri[$x]);
-			$textTitle = "";
-			$text = "";
-			for ($y = 0; $y < count($title); $y++)
-			{
-				$textTitle = " <span class='upperText'>".substr($title[$y], 0, 1)."</span>".substr($title[$y], 1);
-				$text .= $textTitle;
-			}
-		}
-		$menu = explode("-", $uri[1]);
-		$textTitle2 = "";
-		$text2 = "";
-		for ($y = 0; $y < count($menu); $y++)
-		{
-			$textTitle2 = " <span class='upperText'>".substr($menu[$y], 0, 1)."</span>".substr($menu[$y], 1);
-			$text2 .= $textTitle2;
-		}
-	}
-	else{
-		$text = "";
-	}
-?>
 <!-- End of Header -->
--- website/views/scripts/investment/detail.php
+++ website/views/scripts/investment/detail.php
@@ -1,5 +1,33 @@
 <?php echo $this->template("includes/inv/header.php")?>
-
+<?php
+	if(!$this->editmode) { 
+		$url = $_SERVER['REQUEST_URI'];
+		$uri = explode("/", $url);
+		
+		for($x = 1; $x < count($uri); $x++)
+		{
+			$title = explode("-", $uri[$x]);
+			$textTitle = "";
+			$text = "";
+			for ($y = 0; $y < count($title); $y++)
+			{
+				$textTitle = " <span class='upperText'>".substr($title[$y], 0, 1)."</span>".substr($title[$y], 1);
+				$text .= $textTitle;
+			}
+		}
+		$menu = explode("-", $uri[1]);
+		$textTitle2 = "";
+		$text2 = "";
+		for ($y = 0; $y < count($menu); $y++)
+		{
+			$textTitle2 = " <span class='upperText'>".substr($menu[$y], 0, 1)."</span>".substr($menu[$y], 1);
+			$text2 .= $textTitle2;
+		}
+	}
+	else{
+		$text = "";
+	}
+?>
 <div class="main_wrap">
 	
 	<div class="container">
