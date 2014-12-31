df7ba044f830b2355255ecba512b402682b817fc
mrobbin <mrobbi.nugraha@gmail.com>
2014-12-31 19:22:34 +0700

    cookies wizard

--- website/views/scripts/includes/metadata.php
+++ website/views/scripts/includes/metadata.php
@@ -78,18 +78,26 @@ ga('send', 'pageview');
 		});
 
 	})
-	$(document).ready(function(){
-		<?php
-			if(!$this->editmode) {
-				$showWizard = "showWizard";
-				if($_COOKIE[$showWizard] != "Hide") {
-		?>
-					
-						$('#modal-cart').modal("show");
-		<?php 	
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i=0; i<ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') c = c.substring(1);
+	        if (c.indexOf(name) != -1) return c.substring(name.length,c.length);
+	    }
+	    return "";
+	}
+	<?php
+		if(!$this->editmode) {
+	?>
+			$(document).ready(function(){
+				if(getCookie("showWizard") == "") {		
+					$('#modal-cart').modal("show");
 				}
-			}
-		?>
-	});
+			});
+	<?php
+		}
+	?>
 </script>
 <script src="/website/static/js/share-product.js" type="text/javascript"></script>
