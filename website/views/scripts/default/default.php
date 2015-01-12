f4e7753468b40f7e49a05dc3fd384136f07fc6e7
mrobbin <mrobbi.nugraha@gmail.com>
2015-01-12 19:03:18 +0700

    css notepad gallery carousel static home allianz in mobile

--- website/views/scripts/default/default.php
+++ website/views/scripts/default/default.php
@@ -37,7 +37,7 @@
 						$color = $this->select('color_'.$i)->getData();
 					?>
 					<div class="fixbox <?php echo $pos?>60">
-						<div class="place-bg bg-<?php echo $color?>">
+						<div class="place-bg bg-<?php echo $color?> place-bg-static">
 								<?php if($this->editmode || !$this->input("caption-title-" . $i)->isEmpty()) { ?>
 		                            <h1><?php echo $this->input("caption-title-" . $i, ["width" => 251]) ?></h1>
 		                        <?php } ?>
