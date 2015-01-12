4e29073f0f9963ca58dd7299bbdbde18b7694644
mrobbin <mrobbi.nugraha@gmail.com>
2015-01-12 19:14:27 +0700

    css notepad gallery carousel home allianz in mobile

--- website/views/areas/gallery-carousel/view.php
+++ website/views/areas/gallery-carousel/view.php
@@ -59,7 +59,7 @@
 								$color = $this->select('color_'.$i)->getData();
 							?>
 							<div class="fixbox <?php echo $pos?>60">
-								<div class="place-bg bg-<?php echo $color?>">
+								<div class="place-bg bg-<?php echo $color?> place-bg-gallery">
 									<?php if($this->editmode || !$this->input("caption-title-" . $i)->isEmpty()) { ?>
 			                            <h1><?php echo $this->input("caption-title-" . $i, ["width" => 251]) ?></h1>
 			                        <?php } ?>
