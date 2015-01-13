1947c3f3f8512c47e11e151a0d95d79506e61b79
Ihsan Rama <ihsanrama@gmail.com>
2015-01-13 10:39:58 +0700

    Issue No 15, 107, 124, dan 126

--- website/views/scripts/investment/fundinformation.php
+++ website/views/scripts/investment/fundinformation.php
@@ -11,6 +11,61 @@
 	<div class="row">
 		<div class="container">
 
+
+<ul id="membername" class="navi" style="">
+	<li >
+		<a href="/allianz-fund/allianz-fund/fund-information/equity">
+			<div class="nav_menu">
+													
+			<div style="margin-bottom: 3px; width: 25px; height: 25px; display: block; background-image: url(http://www.allianz.co.id/icon_anchor/icon-inactive/tentang_allianz.png);"></div>
+													
+			<div class="white_image" style="margin-bottom: 3px; width: 25px; height: 25px; display: none; background-image: url(http://www.allianz.co.id/icon_anchor/icon-active/tentang-kami.png);"></div>
+													
+			<span class="cus_anchor_0">Equity </span>
+			</div>
+		</a>
+	</li>
+	<li class="aktif">
+		<a href="/allianz-fund/allianz-fund/fund-information/balanced">
+		<div class="nav_menu">
+												
+			<div style="margin-bottom: 3px; width: 25px; height: 25px; display: none; background-image: url(http://www.allianz.co.id/icon_anchor/icon-inactive/tentang_allianz.png);"></div>
+												
+			<div class="white_image" style="margin-bottom: 3px; width: 25px; height: 25px; display: block; background-image: url(http://www.allianz.co.id/icon_anchor/icon-active/tentang-kami.png);"></div>
+												
+			<span class="cus_anchor_1">Balanced </span>
+		</div>
+		</a>
+	</li>
+	
+	<li>
+
+		<a href="/allianz-fund/allianz-fund/fund-information/fixed-income">
+		<div class="nav_menu">
+												
+		<div style="margin-bottom: 3px; width: 25px; height: 25px; display: block; background-image: url(http://www.allianz.co.id/icon_anchor/icon-inactive/tentang_allianz.png);"></div>
+												
+		<div class="white_image" style="margin-bottom: 3px; width: 25px; height: 25px; display: none; background-image: url(http://www.allianz.co.id/icon_anchor/icon-active/tentang-kami.png); "></div>
+												
+		<span class="cus_anchor_3">Fix Income </span>
+		</div>
+		</a>
+	</li>
+	<li>
+		<a href="/allianz-fund/allianz-fund/fund-information/money-market">
+		<div class="nav_menu">
+													
+		<div style="margin-bottom: 3px; width: 25px; height: 25px; display: block; background-image: url(http://www.allianz.co.id/icon_anchor/icon-inactive/tentang_allianz.png);"></div>
+													
+		<div class="white_image" style="margin-bottom: 3px; width: 25px; height: 25px; display: none; background-image: url(http://www.allianz.co.id/icon_anchor/icon-active/tentang-kami.png); "></div>
+													
+		<span class="cus_anchor_4">Money Market </span>
+		</div>
+		</a>
+	</li>
+	
+</ul>
+
 			<?php echo $this->areablock('container', array('allowed' => array('teks-tabs'))) ?>			
 
 		</div>
@@ -51,5 +106,40 @@
 		
 	});
 </script>
+
+
+<div class="row">
+		
+	
+	<div class="row">
+
+			<div class="container">
+
+    			<div class="title_section">
+    			</div>
+
+				<!--<div class="catbox blue">
+					<h2><a href="/allianz-fund/allianz-fund/fund-information">Informasi Fund</a></h2>
+				</div>-->
+
+				<div class="catbox blue">
+					
+					<h2><a href="/allianz-fund/allianz-fund/daily-nav">NAB Harian</a></h2>
+				</div>
+
+				<div class="catbox blue">
+				
+					<h2><a href="/allianz-fund/allianz-fund/fund-fact-sheet">Fund Fact Sheet</a></h2>
+				</div>
+                
+                
+                
+			</div>
+			<!-- container end -->
+
+	</div>
+		
+</div>
+
 
 <?php echo $this->template("includes/inv/footer.php")?>
