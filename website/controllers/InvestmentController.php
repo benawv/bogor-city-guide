f24e9eebd682a8dd9d277a553e61c9afbf8616ac
Fitrazh <fitrazh@gmail.com>
2015-01-13 15:42:24 +0700

    hide local table

--- website/controllers/InvestmentController.php
+++ website/controllers/InvestmentController.php
@@ -437,8 +437,8 @@ class InvestmentController extends Website_Controller_Action
 			 $nameCommunity = "object_query_".$table->getClassId();
 		}
         */
-        //$nameCommunity = "object_query_30";
-        $nameCommunity = "object_query_29";
+        $nameCommunity = "object_query_30";
+        //$nameCommunity = "object_query_29";
         //print_r($nameCommunity);
         // die('mati');
         
@@ -638,8 +638,8 @@ class InvestmentController extends Website_Controller_Action
 			     $nameCommunity = "object_query_".$table->getClassId();
 		      }
 		*/
-	    //$nameCommunity = "object_query_30"; //tulis manual
-            $nameCommunity = "object_query_29"; //tulis manual
+	    $nameCommunity = "object_query_30"; //tulis manual
+            //$nameCommunity = "object_query_29"; //tulis manual
 	    
 	    $sql_subcat="SELECT *,FROM_UNIXTIME(unitdate,'%d-%m-%Y') AS unitdates FROM ".$nameCommunity." AS xmlsource ".$conditions." order by fundname desc,unitdate";
             $xmldata=$db->fetchAll($sql_subcat);
