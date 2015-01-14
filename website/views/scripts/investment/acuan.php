8586dd172d76bf7cc9489cc53851830b0d50a2d7
Ihsan Rama <ihsanrama@gmail.com>
2015-01-14 11:40:54 +0700

    kasih tab pada halaman acuan

--- website/views/scripts/investment/acuan.php
+++ website/views/scripts/investment/acuan.php
@@ -1,6 +1,7 @@
 <style type="text/css">
 body{
     font-family: Arial, sans-serif;
+    padding: 20px;
 }
 </style>
 
@@ -10,98 +11,98 @@ body{
 <hr />
 <h1>Australia</h1>
 <p>
-    Biaya Sekolah : 261.6 <br />
-    Biaya Hidup : 135.3 <br />
-    Total Biaya : 396.9
+    Biaya Sekolah&#09;&#09;: 261.6 <br />
+    Biaya Hidup&#09;&#09;: 135.3 <br />
+    Total Biaya&#09;&#09;: 396.9
 </p>
 <hr />
 
 <h1>Amerika Serikat</h1>
 <p>
-    Biaya Sekolah : 259.8 <br />
-    Biaya Hidup : 107.9 <br />
-    Total Biaya : 367.7
+    Biaya Sekolah&#09;&#09;: 259.8 <br />
+    Biaya Hidup&#09;&#09;: 107.9 <br />
+    Total Biaya&#09;&#09;: 367.7
 </p>
 <hr />
 
 
 <h1>Inggris</h1>
 <p>
-    Biaya Sekolah : 198.7 <br />
-    Biaya Hidup : 113.6 <br />
-    Total Biaya : 312.3
+    Biaya Sekolah&#09;&#09;: 198.7 <br />
+    Biaya Hidup&#09;&#09;: 113.6 <br />
+    Total Biaya&#09;&#09;: 312.3
 </p>
 <hr />
 
 <h1>Uni Emirat Arab</h1>
 <p>
-    Biaya Sekolah : 220.1 <br />
-    Biaya Hidup : 61.8<br />
-    Total Biaya : 281.9
+    Biaya Sekolah&#09;&#09;: 220.1 <br />
+    Biaya Hidup&#09;&#09;: 61.8<br />
+    Total Biaya&#09;&#09;: 281.9
 </p>
 <hr />
 
 <h1>Kanada</h1>
 <p>
-    Biaya Sekolah :190.3 <br />
-    Biaya Hidup :77.6<br />
-    Total Biaya : 267.9
+    Biaya Sekolah&#09;&#09;: 190.3 <br />
+    Biaya Hidup&#09;&#09;: 77.6<br />
+    Total Biaya&#09;&#09;: 267.9
 </p>
 <hr />
 
 <h1>Singapura</h1>
 <p>
-    Biaya Sekolah : 153.3<br />
-    Biaya Hidup : 96.4<br />
-    Total Biaya : 249.7
+    Biaya Sekolah&#09;&#09;: 153.3<br />
+    Biaya Hidup&#09;&#09;: 96.4<br />
+    Total Biaya&#09;&#09;: 249.7
 </p>
 <hr />
 
 <h1>Hongkong</h1>
 <p>
-    Biaya Sekolah : 135.7<br />
-    Biaya Hidup : 95.3<br />
-    Total Biaya : 231.0
+    Biaya Sekolah&#09;&#09;: 135.7<br />
+    Biaya Hidup&#09;&#09;: 95.3<br />
+    Total Biaya&#09;&#09;: 231.0
 </p>
 <hr />
 
 <h1>Jepang</h1>
 <p>
-    Biaya Sekolah : 67.4<br />
-    Biaya Hidup : 130.2<br />
-    Total Biaya : 197.6
+    Biaya Sekolah&#09;&#09;: 67.4<br />
+    Biaya Hidup&#09;&#09;: 130.2<br />
+    Total Biaya&#09;&#09;: 197.6
 </p>
 <hr />
 
 <h1>China</h1>
 <p>
-    Biaya Sekolah : 41.0<br />
-    Biaya Hidup : 49.2<br />
-    Total Biaya : 90.2
+    Biaya Sekolah&#09;&#09;: 41.0<br />
+    Biaya Hidup&#09;&#09;: 49.2<br />
+    Total Biaya&#09;&#09;: 90.2
 </p>
 <hr />
 
 <h1>Taiwan</h1>
 <p>
-    Biaya Sekolah : 33.6<br />
-    Biaya Hidup : 51.3<br />
-    Total Biaya : 84.9
+    Biaya Sekolah&#09;&#09;: 33.6<br />
+    Biaya Hidup&#09;&#09;: 51.3<br />
+    Total Biaya&#09;&#09;: 84.9
 </p>
 <hr />
 
 <h1>Jerman</h1>
 <p>
-    Biaya Sekolah : 6.5<br />
-    Biaya Hidup : 58.2<br />
-    Total Biaya : 64.7
+    Biaya Sekolah&#09;&#09;: 6.5<br />
+    Biaya Hidup&#09;&#09;: 58.2<br />
+    Total Biaya&#09;&#09;: 64.7
 </p>
 <hr />
 
 <h1>Indonesia</h1>
 <p>
-    Biaya Sekolah : 95<br />
-    Biaya Hidup : 50.4<br />
-    Total Biaya : 145.4
+    Biaya Sekolah&#09;&#09;: 95<br />
+    Biaya Hidup&#09;&#09;: 50.4<br />
+    Total Biaya&#09;&#09;: 145.4
 </p>
 <hr />
 
