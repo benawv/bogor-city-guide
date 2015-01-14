215fbf5801c4f8a624e54b667462baa96d4aa5df
Ihsan Rama <ihsanrama@gmail.com>
2015-01-14 11:48:53 +0700

    update tab display pada acuan

--- website/views/scripts/investment/acuan.php
+++ website/views/scripts/investment/acuan.php
@@ -3,6 +3,9 @@ body{
     font-family: Arial, sans-serif;
     padding: 20px;
 }
+span.tab{
+    padding: 0 80px; /* Or desired space*/
+}
 </style>
 
 
@@ -11,98 +14,98 @@ body{
 <hr />
 <h1>Australia</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 261.6 <br />
-    Biaya Hidup&#09;&#09;: 135.3 <br />
-    Total Biaya&#09;&#09;: 396.9
+    Biaya Sekolah <span class="tab"></span>: 261.6 <br />
+    Biaya Hidup <span class="tab"></span>: 135.3 <br />
+    Total Biaya <span class="tab"></span>: 396.9
 </p>
 <hr />
 
 <h1>Amerika Serikat</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 259.8 <br />
-    Biaya Hidup&#09;&#09;: 107.9 <br />
-    Total Biaya&#09;&#09;: 367.7
+    Biaya Sekolah <span class="tab"></span>: 259.8 <br />
+    Biaya Hidup <span class="tab"></span>: 107.9 <br />
+    Total Biaya <span class="tab"></span>: 367.7
 </p>
 <hr />
 
 
 <h1>Inggris</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 198.7 <br />
-    Biaya Hidup&#09;&#09;: 113.6 <br />
-    Total Biaya&#09;&#09;: 312.3
+    Biaya Sekolah <span class="tab"></span>: 198.7 <br />
+    Biaya Hidup <span class="tab"></span>: 113.6 <br />
+    Total Biaya <span class="tab"></span>: 312.3
 </p>
 <hr />
 
 <h1>Uni Emirat Arab</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 220.1 <br />
-    Biaya Hidup&#09;&#09;: 61.8<br />
-    Total Biaya&#09;&#09;: 281.9
+    Biaya Sekolah <span class="tab"></span>: 220.1 <br />
+    Biaya Hidup <span class="tab"></span>: 61.8<br />
+    Total Biaya <span class="tab"></span>: 281.9
 </p>
 <hr />
 
 <h1>Kanada</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 190.3 <br />
-    Biaya Hidup&#09;&#09;: 77.6<br />
-    Total Biaya&#09;&#09;: 267.9
+    Biaya Sekolah <span class="tab"></span>: 190.3 <br />
+    Biaya Hidup <span class="tab"></span>: 77.6<br />
+    Total Biaya <span class="tab"></span>: 267.9
 </p>
 <hr />
 
 <h1>Singapura</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 153.3<br />
-    Biaya Hidup&#09;&#09;: 96.4<br />
-    Total Biaya&#09;&#09;: 249.7
+    Biaya Sekolah <span class="tab"></span>: 153.3<br />
+    Biaya Hidup <span class="tab"></span>: 96.4<br />
+    Total Biaya <span class="tab"></span>: 249.7
 </p>
 <hr />
 
 <h1>Hongkong</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 135.7<br />
-    Biaya Hidup&#09;&#09;: 95.3<br />
-    Total Biaya&#09;&#09;: 231.0
+    Biaya Sekolah <span class="tab"></span>: 135.7<br />
+    Biaya Hidup <span class="tab"></span>: 95.3<br />
+    Total Biaya <span class="tab"></span>: 231.0
 </p>
 <hr />
 
 <h1>Jepang</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 67.4<br />
-    Biaya Hidup&#09;&#09;: 130.2<br />
-    Total Biaya&#09;&#09;: 197.6
+    Biaya Sekolah <span class="tab"></span>: 67.4<br />
+    Biaya Hidup <span class="tab"></span>: 130.2<br />
+    Total Biaya <span class="tab"></span>: 197.6
 </p>
 <hr />
 
 <h1>China</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 41.0<br />
-    Biaya Hidup&#09;&#09;: 49.2<br />
-    Total Biaya&#09;&#09;: 90.2
+    Biaya Sekolah <span class="tab"></span>: 41.0<br />
+    Biaya Hidup <span class="tab"></span>: 49.2<br />
+    Total Biaya <span class="tab"></span>: 90.2
 </p>
 <hr />
 
 <h1>Taiwan</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 33.6<br />
-    Biaya Hidup&#09;&#09;: 51.3<br />
-    Total Biaya&#09;&#09;: 84.9
+    Biaya Sekolah <span class="tab"></span>: 33.6<br />
+    Biaya Hidup <span class="tab"></span>: 51.3<br />
+    Total Biaya <span class="tab"></span>: 84.9
 </p>
 <hr />
 
 <h1>Jerman</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 6.5<br />
-    Biaya Hidup&#09;&#09;: 58.2<br />
-    Total Biaya&#09;&#09;: 64.7
+    Biaya Sekolah <span class="tab"></span>: 6.5<br />
+    Biaya Hidup <span class="tab"></span>: 58.2<br />
+    Total Biaya <span class="tab"></span>: 64.7
 </p>
 <hr />
 
 <h1>Indonesia</h1>
 <p>
-    Biaya Sekolah&#09;&#09;: 95<br />
-    Biaya Hidup&#09;&#09;: 50.4<br />
-    Total Biaya&#09;&#09;: 145.4
+    Biaya Sekolah <span class="tab"></span>: 95<br />
+    Biaya Hidup <span class="tab"></span>: 50.4<br />
+    Total Biaya <span class="tab"></span>: 145.4
 </p>
 <hr />
 
