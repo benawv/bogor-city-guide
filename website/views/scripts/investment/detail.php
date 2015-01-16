b64a2a1e34ba6fcf54b9e9ea0b7043f07310d246
mrobbin <mrobbi.nugraha@gmail.com>
2015-01-16 13:33:51 +0700

    widget investment outlook, annual report

--- website/views/scripts/investment/detail.php
+++ website/views/scripts/investment/detail.php
@@ -45,7 +45,7 @@
 
 		<div class="desc_page">
 			<h1><?php echo $this->input('title')?></h1>
-			<?php echo $this->areablock('referensi-object-pdf', array('allowed' => array('investment-daily')))?>
+			<?php echo $this->areablock('referensi-object-pdf', array('allowed' => array('investment-daily','investment-outlook','investment-ular')))?>
 			<p>
 				<?php echo $this->wysiwyg('description')?>
 			</p>
