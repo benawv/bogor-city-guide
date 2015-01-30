2495d3d53c6a1b3f195553eae29e2a6784151d5e
mrobbin <mrobbi.nugraha@gmail.com>
2015-01-30 11:14:31 +0700

    fund fact Savings

--- website/views/areas/inv-fund-savingsplan/view.php
+++ website/views/areas/inv-fund-savingsplan/view.php
@@ -59,7 +59,7 @@
 													
 		<div class="white_image" style="margin-bottom: 3px; width: 25px; height: 25px; display: block; background-image: url(http://www.allianz.co.id/icon_anchor/icon-active/tentang-kami.png); "></div>
 													
-		<span class="cus_anchor_4">Saving Plan </span>
+		<span class="cus_anchor_4">Savings Plan </span>
 		</div>
 		</a>
 	</li>
@@ -97,7 +97,7 @@
 				<a href="/allianz-fund/allianz-fund/fund-fact-sheet/fund-moneymarket">Money Market</a>
 			</div>
 			<div class="drophide">
-				<a href="/allianz-fund/allianz-fund/fund-fact-sheet/savings-plan">Saving Plan</a>
+				<a href="/allianz-fund/allianz-fund/fund-fact-sheet/savings-plan">Savings Plan</a>
 			</div>
 			<div class="drophide">
 				<a href="/allianz-fund/allianz-fund/fund-fact-sheet/dplk">DPLK</a>
@@ -110,45 +110,26 @@
 <script type="text/javascript">
 	$(function(){
 		$('.year-pick, .month-pick, .type-pick').change(function(){
-			var idFilterSection = $(this).closest('.full-w ').attr('id');
+			//var idFilterSection = $(this).closest('.full-w ').attr('id');
             	      
 			var year = $('.year-pick').val(); //$('#'+idFilterSection + " .year-pick").val(); 
-			year = year.substr();
+			//year = year;
            // alert(year);
             var month = $('.month-pick').val();//$('#'+idFilterSection + " .month-pick").val();	                        
 		//	alert(month);
-            
+            var typeData = $(".type-pick").val();
             var x = 0;
             
             
-			$(" .description li").each(function(){
+			$(".description li").each(function(){
 				var itemAttr = $(this).find('a').attr('item');
 				splitedItem = itemAttr.split('/');
               //  alert('bulan'+splitedItem[1]+'-'+'tahun'+splitedItem[2]);
 				var visible = true;
-				
-                if(month==splitedItem[1]){
-                    visible2=true;
-                }else if(month==' '){
-                    visible2=true;
-                }else{
-                    visible2=false;
-                }
-               // alert(visible2);
-                 visible = visible && visible2;
-                 
+		visible = visible && (month == '' || month == splitedItem[0]);
+		visible = visible && (year == '' || year == splitedItem[1]);
+		visible = visible && (typeData  == '' || typeData == splitedItem[2]);
                 
-                if(year==splitedItem[2]){
-                    visible3=true;
-                }else if(year==' '){
-                     visible3=true;
-                }else{
-                    visible3=false;
-                }
-                //alert(visible3);
-                 visible = visible && visible3;
-               
-                //console.log(visible);
 
 				if(visible==true){
 					$(this).fadeIn('slow');
@@ -176,16 +157,15 @@
 		{
 
 			$idAssets = $row1->id;
-			$list_year = new Asset_List(); 
-			$list_year ->setCondition("parentId = '".$idAssets."'");
+			$list_tipe = new Asset_List(); 
+			$list_tipe ->setCondition("parentId = '".$idAssets."'");
 			$year = array();
 			$list = array();
 			
             
-			foreach($list_year  as $row2)
+			foreach($list_tipe  as $row2)
 			{
 		 
-                $years[] = $row2->filename;
 				$idJenis = $row2->id;
 				$tahun = new Asset_List();
 				$tahun->setCondition("parentId = '".$idJenis."'");
@@ -201,15 +181,20 @@
 					foreach($bulan as $row4)
 					{
 						$idBulan = $row4->id;
-                        
-                            $item = $row4->filename."/".substr($row3->filename, 0)."/".$row2->filename;
-							$list[] = '<li><a item="'.$item.'" target="_blank" href="'.$row4->path."".$row4->filename.'">'.$row4->filename.'</a></li>';
+						
+						$data = new Asset_List();
+						$data->setCondition("parentId = '".$idBulan."'");
+						foreach($data as $isi)
+						{
+							$item = $row4->filename."/".substr($row3->filename, 0)."/".$row2->filename;
+							$list[] = '<li><a item="'.$item.'" target="_blank" href="'.$isi->path."".$isi->filename.'">'.$isi->filename.'</a></li>';
+						}
 					}
 				}
 			}
 			$full_data = $list;
-			$Listtahun = array_unique($year);
-			asort($Listtahun);
+			$list_year = array_unique($year);
+			asort($list_year);
 		}
 	?>
 	<div class="combo_section">
@@ -218,9 +203,9 @@
 			<select class="combo-width year-pick" style="color: black;">
 				<option value="" selected="selected">--Pilih Semua--</option>
 				<?php 
-					foreach($list_year  as $row)
+					foreach($list_year  as $key => $row)
 					{
-						$namaJenis = $row->filename;
+						$namaJenis = $row;
 						echo '<option value="'.$namaJenis.'">'.strtoupper($namaJenis).'</option>';
 					}
 				?>
@@ -229,7 +214,7 @@
 		<p>
 			Pilih Bulan<br />
 			<select class="combo-width month-pick" style="color: black;">
-				<option value=" " selected="selected">--Pilih Semua--</option>
+				<option value="" selected="selected">--Pilih Semua--</option>
                 <option value="januari">Januari</option>
 				<option value="februari">Februari</option>
 				<option value="maret">Maret</option>
@@ -251,7 +236,20 @@
 				?>
 			</select>
 		</p>
-		
+		<p>
+			Pilih Tipe<br />
+			<select class="combo-width type-pick" style="color: black;">
+				<option value='' selected="selected">--Pilih Semua--</option>
+				<?php 
+					foreach($list_tipe  as $row)
+					{
+						$namaJenis = $row->filename;
+						$display = str_replace("-", " ", $namaJenis);
+						echo '<option value="'.$namaJenis.'">'.ucwords(strtolower($display)).'</option>';
+					}
+				?>
+			</select>
+		</p>
 		
 	</div>
 	<div class="description fundfact">
