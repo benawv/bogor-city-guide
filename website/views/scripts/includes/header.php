43a8ffffd56c7f9f67638d66ec146c207af4066b
mrobbin <mrobbi.nugraha@gmail.com>
2015-02-24 14:16:13 -0800

    live - font awesome

--- website/views/scripts/includes/header.php
+++ website/views/scripts/includes/header.php
@@ -31,7 +31,7 @@ function check_user_agent ( $type = NULL ) {
         return false;
 }
 ?>
-<link href="/website/static/css/font-awesome/font-awesome.min.css" rel="stylesheet">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
 <script type="text/javascript">var switchTo5x=true;</script>
 <!--script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script!-->
 <script type="text/javascript">stLight.options({publisher: "16852030-1a9b-4882-8731-4afd4c1cfd9c", doNotHash: false, doNotCopy: false, hashAddressBar: false});</script>
