0c8f0ca3f608587ee2f80a6493f0bd61c2e3ebd8
mrobbin <mrobbi.nugraha@gmail.com>
2015-02-25 18:29:54 -0800

    image full page

--- website/views/areas/image-full-page/view.php
+++ website/views/areas/image-full-page/view.php
@@ -1,7 +1,14 @@
 <div class="full-w bg-white">
 	<div class="description">
 		<div>
-			<?php echo $this->image('image-full-page')?>
+			<?php if($this->editmode) { ?>
+				<?php echo $this->image('image-full-page', array(
+					"min-width" => '100%',
+					"min-height" => 500)
+				);?>
+			<?php } else { ?>
+				<?php echo $this->image('image-full-page');?>
+			<?php } ?>
 		</div>
 	</div>
 </div>
