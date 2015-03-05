93abd606777d93e68975b33c5c67ca33f4fca9da
mrobbin <mrobbi.nugraha@gmail.com>
2015-03-05 08:04:08 +0700

    test kuis 3

--- website/controllers/KuisController.php
+++ website/controllers/KuisController.php
@@ -60,7 +60,7 @@ class KuisController extends Website_Controller_Action {
 		$db = Pimcore_Resource_Mysql::get();
 		
 		$peserta = new Object_DataPesertaAJFC_List();
-		$peserta->setCondition("(`idPeserta` = '' OR `idPeserta` IS NULL) AND (`statusEmail` <> 1 OR `statusEmail` IS NULL) ");
+		$peserta->setCondition("`idPeserta` = '' OR `idPeserta` IS NULL AND `statusEmail` <> 1 OR `statusEmail` IS NULL ");
 		$peserta->setLimit(10);
 		
 		//echo strtotime(date('Y-m-d')), "\n";
