803de332018c7317ca75569f87938a519da03acb
Fitrazh <fitrazh@gmail.com>
2015-03-16 16:11:24 +0700

    email->username ; asn

--- website/views/scripts/includes/header.php
+++ website/views/scripts/includes/header.php
@@ -214,10 +214,13 @@ $(function(){
 		      <div class="modal-content" style="padding: 20px">
 			      <form class="form-signin" action="https://www.allianzstarnetwork.com/redirect.php" method="POST">
 				      <h2 class="form-signin-heading"><b>ASN Sign In</b></h2>
-				      <label for="inputEmail" class="sr-only">Email address</label>
-				      <input type="input" id="inputEmail" class="form-control popup-asn-label" placeholder="Username" required autofocus style="margin-bottom:10px">
-				      <label for="inputPassword" class="sr-only">Password</label>
-				      <input type="password" id="inputPassword" class="form-control popup-asn-label" placeholder="Password" required style="margin-bottom:10px">
+				      
+				      <label for="username" class="sr-only">Username</label>
+				      <input type="input" id="username" name="login_username" title="login_username" tabindex="4" class="input_login form-control popup-asn-label" placeholder="Username" required autofocus style="margin-bottom:10px">
+				      
+				      <label for="password" class="sr-only">Password</label>
+				      <input type="password" id="password" name="secretkey" title="password" tabindex="5" class="input_login form-control popup-asn-label" placeholder="Password" maxlength="15" required autofocus>
+				      
 				      <input type="hidden" name="js_autodetect_results" value="0">
 				      <input type="hidden" name="just_logged_in" value="1">	
 				      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
