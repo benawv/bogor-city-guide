51a77550ab658260c72f66b887ef3264abfe5a28
mrobbin <mrobbi.nugraha@gmail.com>
2015-03-17 14:40:53 +0700

    accordion page FAQ

--- website/views/areas/table-text/view.php
+++ website/views/areas/table-text/view.php
@@ -16,8 +16,46 @@
 		<?php echo $this->input('title', ["width" => 250])?>
 	</h1>
 	<div class="description cusTable">
+	
+	<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css-mobil/bootstrap.min.css"  />
+	<!--<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+		<div class="panel panel-default">
+		  <div class="panel-heading" role="tab" id="headingOne">
+		    <h4 class="panel-title">
+		      <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+			Bagaimana cara mengikuti  program Allianz Junior Football Camp?
+		      </a>
+		    </h4>
+		  </div>
+		  <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+		    <div class="panel-body">
+		      Daftar online dan mengikuti rangkaian seleksi di www.allianz.co.id/juniorfootballcamp dibuka dari tanggal 17 April 2015* sampai 22 Mei 2015*.
+Setelah mendaftar, setiap peserta akan dikirimkan 1 kali kuis online pada hari JUMAT mendatang ke email masing-masing. Kuis tersebut hanya berlaku 3 hari sejak email diterima oleh peserta.
+Pengiriman soal kuis online akan berlangsung dari 17 April 2015*sampai dengan 22 Mei 2015*.
+		    </div>
+		  </div>
+		</div>
+		<div class="panel panel-default">
+		  <div class="panel-heading" role="tab" id="headingTwo">
+		    <h4 class="panel-title">
+		      <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+			Collapsible Group Item #2
+		      </a>
+		    </h4>
+		  </div>
+		  <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+		    <div class="panel-body">
+		      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		    </div>
+		  </div>
+		</div>
+	</div>-->
+		
+		
+		
 		<?php if(!$this->editmode) { ?>
-			<table>
+			<!--<table>-->
+			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 		<?php }
 			$x = 1;
 		?>
@@ -25,18 +63,51 @@
 			<?php if($this->editmode) { ?>
 				<ul>
 					<li>
+						<?php echo $this->input('title_faq')?>
 						<?php echo $this->wysiwyg('description')?>
 					</li>
 				</ul>
 			<?php } else { ?>
-				<tr>
+				<!--<tr>
 					<td class="vertical"><?php echo $x.".";?></td>
 					<td><?php echo $this->wysiwyg('description')->text;?></td>
-				</tr>
+				</tr>-->
+				<?php if($x==1) {?>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="heading<?php echo $x;?>">
+					  <h4 class="panel-title">
+					    <a data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $x;?>" aria-expanded="true" aria-controls="collapseOne">
+					      <?php echo $x.". ".$this->input('title_faq')->text;?>
+					    </a>
+					  </h4>
+					</div>
+					<div id="collapse<?php echo $x;?>" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+					  <div class="panel-body">
+					    <?php echo $this->wysiwyg('description')->text;?>
+					  </div>
+					</div>
+				</div>
+				<?php } else {?>
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="heading<?php echo $x;?>">
+					  <h4 class="panel-title">
+					    <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapse<?php echo $x;?>" aria-expanded="false" aria-controls="collapseTwo">
+					      <?php echo $x.". ".$this->input('title_faq')->text;?>
+					    </a>
+					  </h4>
+					</div>
+					<div id="collapse<?php echo $x;?>" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+					  <div class="panel-body">
+					    <?php echo $this->wysiwyg('description')->text;?>
+					  </div>
+					</div>
+				</div>
+				<?php }?>
 			<?php $x++; } ?>
 		<?php } ?>
 		<?php if(!$this->editmode) { ?>
-			</table>
+			<!--</table>-->
+			</div>
 		<?php }?>
 		<br />
 		<?php echo $this->wysiwyg('description_2')?>
