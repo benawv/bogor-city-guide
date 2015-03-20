8d20603090b2bd00de4f1f5d25295b79bd7500cc
Fitrazh <fitrazh@gmail.com>
2015-03-20 20:06:05 +0700

    test die final

--- website/controllers/InvestmentController.php
+++ website/controllers/InvestmentController.php
@@ -795,10 +795,10 @@ class InvestmentController extends Website_Controller_Action
 	
 //	$day=$_POST['day'];
 //	$month=$_POST['month'];
-//        $year=$_POST['year'];
+//      $year=$_POST['year'];
 //	
-	$day=3;
-	$month=3;
+	$day=2;
+	$month=1;
         $year=2015;
 	$getlastunitdate= $year."-".$month."-".$day;	
 	
@@ -808,8 +808,8 @@ class InvestmentController extends Website_Controller_Action
 	
 	$getLast3m="SELECT a.fundname,a.bid,a.offer,DATE_ADD(STR_TO_DATE(FROM_UNIXTIME(a.unitdate,'%d-%m-%Y'), '%d-%m-%Y'), INTERVAL 1 DAY) AS last_3_month
                         FROM object_query_30 AS a
-                        WHERE fundname LIKE '%AlliSya  Rupiah Equity Fund%' AND 
-                        DATE_ADD(STR_TO_DATE(FROM_UNIXTIME(a.unitdate,'%d-%m-%Y'), '%d-%m-%Y'), INTERVAL 1 DAY) >= DATE_ADD(STR_TO_DATE('2015-1-2','%Y-%m-%d'), INTERVAL- 1 DAY)
+                        WHERE fundname LIKE '%$fundtype%' AND 
+                        DATE_ADD(STR_TO_DATE(FROM_UNIXTIME(a.unitdate,'%d-%m-%Y'), '%d-%m-%Y'), INTERVAL 1 DAY) >= DATE_ADD(STR_TO_DATE('$getlastunitdate','%Y-%m-%d'), INTERVAL- 1 DAY)
                         ORDER BY a.unitdate DESC";
 	$last3mData=$db->fetchAll($getLast3m);
 	print_r($last3mData);
