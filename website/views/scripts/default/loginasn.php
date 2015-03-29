0c9c6c10ae9f6143b3359098187226b7677d77be
mrobbin <mrobbi.nugraha@gmail.com>
2015-03-29 17:02:49 +0700

    live - login asn

---
+++ website/views/scripts/default/loginasn.php
@@ -0,0 +1,20 @@
+<div class="popup-asn">
+            <div class="modal-dialog">
+                    <div class="modal-content" style="padding: 20px">
+                            <form class="form-signin" action="https://www.allianzstarnetwork.com/redirect.php" method="POST">
+                                    <h2 class="form-signin-heading"><b>ASN Sign In</b></h2>
+                                    
+                                    <!--label for="username" class="sr-only">Username</label-->
+                                    <input type="input" id="username" name="login_username" title="login_username" tabindex="4" class="input_login form-control popup-asn-label" placeholder="Username" required autofocus style="margin-bottom:10px">
+                                    
+                                    <!--label for="password" class="sr-only">Password</label-->
+                                    <input type="password" id="password" name="secretkey" title="password" tabindex="5" class="input_login form-control popup-asn-label" placeholder="Password" maxlength="15" required autofocus>
+                                    
+                                    <input type="hidden" name="js_autodetect_results" value="0">
+                                    <input type="hidden" name="just_logged_in" value="1">
+                                      <br />
+                                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                            </form>
+                    </div>
+            </div>	
+ </div>
