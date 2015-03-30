3729c847de1334a9dc891d78ea71d321f0a1b7ea
mrobbin <mrobbi.nugraha@gmail.com>
2015-03-30 22:44:41 +0700

    test list berita home

---
+++ website/views/areas/static-banner-home/view.php
@@ -0,0 +1,66 @@
+<div style="overflow: hidden; position: relative;" class="flex-viewport">
+<ul style="width: 1600%; transition-duration: 0.6s; transform: translate3d(-2540px, 0px, 0px);" class="slides">
+
+<li style="width: 635px; float: left; display: block;" class="">
+	<div class="product">
+		<div class="photo">
+			<?php $i = "static";$id = "idStatic";
+				  echo $this->image("image_".$i, ["thumbnail" => "galleryCarouselStatic", "dropClass" => $id . "-" . $i, 'title' 	=> 'Image Size 635x310']);
+			?>
+		</div>
+		<?php
+			$extra = $this->image("image_".$i)->getHotspots();
+			//$pos = $extra[0]['data'][0]['value'];
+			//$color = $extra[0]['data'][1]['value'];
+			$pos = $this->select('position_'.$i)->getData();
+			$color = $this->select('color_'.$i)->getData();
+		?>
+		<div class="fixbox <?php echo $pos?>60">
+			<div class="place-bg bg-<?php echo $color?>">
+					<?php if($this->editmode || !$this->input("caption-title-" . $i)->isEmpty()) { ?>
+		    <h1><?php echo $this->input("caption-title-" . $i, ["width" => 251]) ?></h1>
+		<?php } ?>
+		<?php if($this->editmode || !$this->textarea("caption-text-" . $i)->isEmpty()) { ?>
+		    <p>
+			<?php echo $this->textarea("caption-text-" . $i, ["width" => 251, "height" => 100]) ?>
+		    </p>
+		<?php } ?>
+		<?php if($this->editmode) { ?>
+			<p>
+			<?php 
+				echo "Position: <br />";
+				echo $this->select("position_".$i,array(
+							    "store" => array(
+								array("left", "Left"),
+								array("right", "Right")
+							    )
+							)); 
+						?>
+			</p>
+			<p>
+			<?php 
+				echo "Color: <br />";
+				echo $this->select("color_".$i,array(
+							    "store" => array(
+								array("red", "Red"),
+								array("lightgreen", "Light Green"),
+								array("purple", "Purple"),
+								array("blue", "Blue"),
+								array("orange", "Orange")
+							    ),
+							    "reload" => true
+							)); 
+						?>
+			</p>
+		<?php } ?>
+				</div>
+				<div class="edge e-<?php echo $color?>">
+					<?php echo $this->link("boxlink_".$i); ?>
+				</div>
+			</div>
+	</div>
+</li>
+		
+		
+</ul>
+</div>
--- website/views/scripts/default/default.php
+++ website/views/scripts/default/default.php
@@ -4,87 +4,8 @@
 	<div class="wrapper-special clearfix noArrow">
 		<?php /* echo $this->areablock('bannerProduct', array(
 				'allowed' => array('banner', 'static-banner'))) */?>
-		<div id="best-products">
-			<div style="overflow: hidden; position: relative;" class="flex-viewport">
-			<ul style="width: 1600%; transition-duration: 0.6s; transform: translate3d(-2540px, 0px, 0px);" class="slides">
-
-			<li style="width: 635px; float: left; display: block;" class="">
-				<div class="product">
-					<!-- <div class="photo">
-						<img draggable="false" alt="" src="/website/static/images/static_image/image_static_allianz_global.png" height="380" width="635">
-					</div>
-					<div class="fixbox right60">
-						<div class="place-bg bg-blue">
-								<h1>Allianz Global Benefits</h1>
-								<p>
-                                	Allianz Global Benefits helps multinational corporations to optimize their employee benefit programs across countries.
-                                </p>
-						</div>
-						<div class="edge e-blue">
-							<a href="/produk/asuransi-kesehatan/allianz-global">Info selengkapnya</a>
-						</div>
-					</div> -->
-					<div class="photo">
-						<?php $i = "static";$id = "idStatic";
-							  echo $this->image("image_".$i, ["thumbnail" => "galleryCarouselStatic", "dropClass" => $id . "-" . $i, 'title' 	=> 'Image Size 635x310']);
-						?>
-					</div>
-					<?php
-						$extra = $this->image("image_".$i)->getHotspots();
-						//$pos = $extra[0]['data'][0]['value'];
-						//$color = $extra[0]['data'][1]['value'];
-						$pos = $this->select('position_'.$i)->getData();
-						$color = $this->select('color_'.$i)->getData();
-					?>
-					<div class="fixbox <?php echo $pos?>60">
-						<div class="place-bg bg-<?php echo $color?>">
-								<?php if($this->editmode || !$this->input("caption-title-" . $i)->isEmpty()) { ?>
-		                            <h1><?php echo $this->input("caption-title-" . $i, ["width" => 251]) ?></h1>
-		                        <?php } ?>
-		                        <?php if($this->editmode || !$this->textarea("caption-text-" . $i)->isEmpty()) { ?>
-		                            <p>
-		                                <?php echo $this->textarea("caption-text-" . $i, ["width" => 251, "height" => 100]) ?>
-		                            </p>
-		                        <?php } ?>
-		                        <?php if($this->editmode) { ?>
-		                        	<p>
-		                        	<?php 
-		                        		echo "Position: <br />";
-		                        		echo $this->select("position_".$i,array(
-										    "store" => array(
-										        array("left", "Left"),
-										        array("right", "Right")
-										    )
-										)); 
-									?>
-		                        	</p>
-		                        	<p>
-			                        <?php 
-			                        	echo "Color: <br />";
-		                        		echo $this->select("color_".$i,array(
-										    "store" => array(
-										        array("red", "Red"),
-										        array("lightgreen", "Light Green"),
-										        array("purple", "Purple"),
-										        array("blue", "Blue"),
-										        array("orange", "Orange")
-										    ),
-										    "reload" => true
-										)); 
-									?>
-		                        	</p>
-		                        <?php } ?>
-							</div>
-							<div class="edge e-<?php echo $color?>">
-								<?php echo $this->link("boxlink_".$i); ?>
-							</div>
-						</div>
-				</div>
-			</li>
-					
-					
-			</ul>
-			</div>
+		<div id="best-products" class="banner_products" style="min-height: 100px;">
+			<?php echo $this->areablock('static_banner', array('allowed' => array('static-banner-home','list-berita')))?>
 		</div>
 		<div id="quicklinks">
 			<?php echo $this->areablock('layananKami', array(
