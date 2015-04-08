18c8dd81fc41a3999fdcfc5cba3c8b612aa5d74b
Falmesino Abdul Hamid <falmesino@gmail.com>
2015-04-08 12:58:05 +0700

    change home navigation from 4 to 3 columns

--- website/ajfc/includes/page-home.php
+++ website/ajfc/includes/page-home.php
@@ -34,7 +34,7 @@
             <div class="row">
                 <div class="col-xs-12 col-md-4">
                     <a href="?p=proses-seleksi" class="nav-item green sitemap">
-                        <h4><small>01</small> Mekanisme.</h4>
+                        <h4><small>01</small> Proses Seleksi</h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
                 <!--
@@ -45,12 +45,12 @@
                 </div><!--/ .col-xs-12 -->
                 <div class="col-xs-12 col-md-4">
                     <a href="?p=faq" class="nav-item orange question">
-                        <h4><small>03</small> FAQ.</h4>
+                        <h4><small>02</small> Tanya Jawab</h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
                 <div class="col-xs-12 col-md-4">
                     <a href="?p=daftar-peserta" class="nav-item users">
-                        <h4><small>04</small> Daftar Peserta.</h4>
+                        <h4><small>03</small> Sudah Mendaftar? <span>Cek Disini</span></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
             </div><!--/ .row -->
