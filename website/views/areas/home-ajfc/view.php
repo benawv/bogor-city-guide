ef7d1f6b4632a0ab863eb3945153c8b75e570c98
mrobbin <mrobbi.nugraha@gmail.com>
2015-04-09 10:33:46 +0700

    pendaftaran

--- website/views/areas/home-ajfc/view.php
+++ website/views/areas/home-ajfc/view.php
@@ -12,8 +12,18 @@
                             <h2>AJFC 2015</h2>
                             <p>#1ygterpenting mewujudkan mimpi menjadi kenyataan.</p>
 			    <p>Daftarkan anak/kerabat Anda yang berusia 14-16 tahun untuk dilatih oleh pelatih remaja FC Bayern Munchen di Jerman.</p>
-                            <a href="#">Daftar Sekarang <i class="fa fa-angle-right"></i></a>
-                        </div><!--/ .box-dent--inner -->
+                            <?php
+				if($this->editmode)
+				{
+					echo $this->link("linkDaftar");
+				}
+				else{
+			    ?>
+				<a href="<?php echo $this->link("linkDaftar")->getHref();?>"><?php echo $this->link("linkDaftar")->getText();?></a>
+			    <?php
+				}
+			    ?>
+			</div><!--/ .box-dent--inner -->
                     </div>
                 </div><!--/ .col-xs-6 -->
             </div><!--/ .row -->
