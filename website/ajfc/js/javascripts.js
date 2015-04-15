d6ef85192b2a569776bf2395172eb49e21117e0d
Falmesino Abdul Hamid <falmesino@gmail.com>
2015-04-15 10:50:46 +0700

    remove unused console.log()s

--- website/ajfc/js/javascripts.js
+++ website/ajfc/js/javascripts.js
@@ -139,7 +139,6 @@ function resizePageWrapper( additionalHeight )
     if( $( '.page-wrapper-outer > .page-wrapper' ).length > 0 )
     {
         var windowWidth = $( window ).outerWidth();
-        // console.log( windowWidth );
         var refHeight = $( '.page-wrapper-outer > .page-wrapper' ).outerHeight();
 
         if( $( '.page-wrapper-outer > .page-wrapper' ).hasClass( 'gallery-wrapper' ) )
@@ -158,7 +157,6 @@ function resizePageWrapper( additionalHeight )
                 if( $( '.page-wrapper-outer > .page-wrapper .sidebar' ).length > 0 )
                 {
                     var sidebarHeight = $( '.page-wrapper-outer > .page-wrapper .sidebar' ).outerHeight();
-                    // console.log( 'Ada sidebarnya, tingginya ' + sidebarHeight );
                     refHeight = refHeight + ( 1 * sidebarHeight );
                 }
             }
@@ -167,8 +165,6 @@ function resizePageWrapper( additionalHeight )
         $( '.page-wrapper-outer' ).stop().animate({
             'min-height': newHeight + 'px'
         }, 300);
-        // console.log( 'refHeight: ' + refHeight + 'px' );
-        // console.log( 'newHeight: ' + newHeight + 'px' );
     }
 }
 
