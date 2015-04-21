0757e4ff1ee7b6bafad5291524fb637f35a2fd8c
Fitrazh <fitrazh@gmail.com>
2015-04-21 13:56:33 +0700

    hide static title

--- website/views/scripts/includes/inv/header.php
+++ website/views/scripts/includes/inv/header.php
@@ -3,7 +3,7 @@
 <head>
 <!-- Meta Tags -->
 <meta charset="utf-8">
-<title>Allianz Investment</title>
+<!--title>Allianz Investment</title-->
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <meta name="apple-mobile-web-app-capable" content="yes">
