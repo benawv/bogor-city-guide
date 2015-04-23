82ce8b9ed1fe5830eca63fa27bdfbda7f93ad713
Falmesino Abdul Hamid <falmesino@gmail.com>
2015-04-23 15:07:30 +0700

    tasbih new layout

--- website/views/areas/tasbih/view.php
+++ website/views/areas/tasbih/view.php
@@ -1,154 +1,153 @@
-   
-<link rel="stylesheet" href="/website/static/mobilku/tasbih/tasbih.css" type="text/css" media="all" /> 
-<link rel="stylesheet" href="/website/static/mobilku/wizard-step.css" type="text/css" media="all" /> 
-
 <link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/mobilku/bootstrap.min.css"  />
 <link rel="stylesheet" type="text/css" media="screen" id="normalize-css" href="/website/static/css/normalize.css">
 <link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/screen.css">
-<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="_assets/css/flexslider.css">-->
 <link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/main.css">
+
+<link rel="stylesheet" type="text/css" media="all" href="/website/static/mobilku/tasbih/tasbih.css" />
+<link rel="stylesheet" type="text/css" media="all" href="/website/static/mobilku/wizard-step.css" />
+
+<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="_assets/css/flexslider.css">-->
 <!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="_assets/css/tabs-accordion.css">-->
-<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/product.css">-->     	
-  
+<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/product.css">-->
+
 <script src="/website/static/js/masonry.min.js" type="text/javascript"></script>
 
-		
-<!--   plugins 	 -->
+<div id="newsletter-allianz" class="full-w bg-white nopadding">
+    <div class="description">
 
+        <div class="header-calc">
+            <h1>KALKULATOR ASURANSI ALLIANZ TASBIH</h1>
+        </div><!--/ .header-calc -->
 
-<!-- End of Header -->
+        <div class="content-calc">
+            <div class="col-md-12">
 
+                <form class="form-horizontal">
 
-		<div id="newsletter-allianz" class="full-w bg-white nopadding">
-			
-			<div class="description">
-				
-               <div class="header-calc">
-                  <h1>KALKULATOR ASURANSI ALLIANZ TASBIH</h1>
-               </div>
-
-               <div class="content-calc">
-                  <div class="col-md-12">
-                      <form class="form-horizontal">
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label><strong>Tanggal Pembuatan Perhitungan</strong></label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label><strong>Tanggal Pembuatan Perhitungan</strong></label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="text" class="form-control input-perhitungan" id="tgl-hitung" placeholder="">
-                          </div>
-                        </div>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
 
-                        <label><strong>Data Diri</strong></label>
+                    <label><strong>Data Diri</strong></label>
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Nama</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Nama</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="nama" placeholder="">
-                          </div>
-                        </div>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Tanggal Lahir</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Tanggal Lahir</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="tgl-lahir" placeholder="">
-                          </div>
-                        </div>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Jenis Kelamin</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Jenis Kelamin</label>
+                        </div>
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="jenis-kelamin" placeholder="">
-                          </div>
                         </div>
+                    </div><!--/ .form-group -->
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Usia (Tahun)</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Usia (Tahun)</label>
+                        </div>
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="usia" placeholder="">
-                          </div>
                         </div>
+                    </div><!--/ .form-group -->
 
-                         <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Frekuensi Pembayaran</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Frekuensi Pembayaran</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="email" class="form-control mb10" id="frekuensi" placeholder="">
                             <span>1. Tahunan</span>
                             <span>2. Semesteran</span>
                             <span>3. Triwulanan</span>
-                          </div>
-                        </div>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
 
-                        <label><strong>Santunan Meninggal Dunia</strong></label>
+                    <label><strong>Santunan Meninggal Dunia</strong></label>
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Asuransi Jiwa (Min. Rp.50.000.000)</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Asuransi Jiwa (Min. Rp.50.000.000)</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="asuransi-jiwa" placeholder="">
-                          </div>
-                        </div>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Massa Pembayaran Kontribusi/ Premi</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Massa Pembayaran Kontribusi/ Premi</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="masa-premi" placeholder="">
-                          </div>
-                        </div>
+                        </div><!--/ .col-md-4 --><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
 
-                        <label><strong>Setoran Tahunan</strong></label>
+                    <label><strong>Setoran Tahunan</strong></label>
 
-                        <div class="form-group">
-                          <div class="col-md-4">
-                              <label>Kontribusi Berkala/ Premi pertahun</label>
-                          </div>
-                          <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Kontribusi Berkala/ Premi pertahun</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
                             <input type="email" class="form-control" id="kontribusi-berkala" placeholder="">
-                          </div>
-                        </div>
-                        
-                        <div class="form-group">
-                          <div class="col-md-12">
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+
+                    <div class="form-group">
+                        <div class="col-md-12">
                             <input type="button" class="btn btn-next btn-fill btn-warning btn-wd btn-sm pull-right" name="next" value="Kalkulasi">
-                          </div>
-                        </div>
-                      </form> 
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+                </form>
 
-                      <div class="col-md-12 note">
-                          <h4 class="mb10"><strong>Note: </strong></h4>
-                          <ul class="mb30">
-                              <li>Jika meninggal dunia karena sakit, Ahli waris akan mendapatkan Santunan meninggal Dunia 100%</li>
-                              <li>Jika meninggal dunia di saat menunaikan Ibadah Haji maka akan mendapatkan Santunan meninggal Dunia sebesar 200%</li>
-                              <li>Selama mengikuti asuransi Tertanggung akan mendapatkan perlindungan Medical Evakuasi</li>
-                              <li>Masa asuransi adalah = masa pembayaran konstribusi / premi + 10 tahun + 1 tahun</li>
-                              <li>Setelah selesai masa pembayaran kontribusi / premi pemegang polis akan mendapatkan tahapan dana pertama sebesar 50% dari santunan meninggal dunia</li>
-                              <li>Setelah tahapan dana pertama pemegang polis akan mendapatkan tahapan dana kedua sebesar sisa saldo dana yang berkembang didalam produk ini</li>
-                              <li>Untuk informasi lebih lengkap silahkan menghubungi Agen atau Kantor pemasaran Allianz terdekat</li>
-                          </ul>    
-                      </div>
-                  </div>    
-               </div>
-                
-			</div>
-			
-		</div>
-		<!-- newsletter-allianz end -->
-			
-			
-</div>
+                <div class="col-md-12 note">
+                    <h4 class="mb10"><strong>Note: </strong></h4>
+                    <ul class="mb30">
+                        <li>Jika meninggal dunia karena sakit, Ahli waris akan mendapatkan Santunan meninggal Dunia 100%</li>
+                        <li>
+                            Jika meninggal dunia di saat menunaikan Ibadah Haji maka akan mendapatkan Santunan
+                            meninggal Dunia sebesar 200%
+                        </li>
+                        <li>Selama mengikuti asuransi Tertanggung akan mendapatkan perlindungan Medical Evakuasi</li>
+                        <li>Masa asuransi adalah = masa pembayaran konstribusi / premi + 10 tahun + 1 tahun</li>
+                        <li>
+                            Setelah selesai masa pembayaran kontribusi / premi pemegang polis akan mendapatkan
+                            tahapan dana pertama sebesar 50% dari santunan meninggal dunia
+                        </li>
+                        <li>
+                            Setelah tahapan dana pertama pemegang polis akan mendapatkan tahapan dana
+                            kedua sebesar sisa saldo dana yang berkembang didalam produk ini
+                        </li>
+                        <li>Untuk informasi lebih lengkap silahkan menghubungi Agen atau Kantor pemasaran Allianz terdekat</li>
+                    </ul>
+                </div><!--/ .col-md-12 -->
 
-    
-  
+            </div><!--/ .col-md-12 -->
+        </div><!--/ .content-calc -->
+    </div><!--/ .description -->
+</div><!--/ #newsletter-allianz -->
+
+<!--
+</div> LEBIH SATU
+-->
