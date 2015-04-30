d2f03993d1348b2487237e9434653cf7d425e0eb
BastianRDP <bastianrdp@gmail.com>
2015-04-30 11:53:53 +0700

    try

--- website/controllers/MobilkuController.php
+++ website/controllers/MobilkuController.php
@@ -47,12 +47,11 @@ class MobilkuController extends Website_Controller_Action {
         }
         
 		$getId=Object_Abstract::getByPath('/kalkulator/personal-lines');//get folder id
-		$entries = Object_Abstract::getByID($model);
-       //echo $entries;
+		$entries = Object_Abstract::getById($model);
 		$register = new Object_MobilPersonalLines();
 		$register->setyear($th);
 		$register->setprice($hargaKonv);
-		$register->setmodel($entries->o_id);		
+		$register->setmodel($entries);		
 		$register->setregistrationno($regno);
 		$register->setstartperiode($date_tglPeriod);
 		$register->setemail($email);
@@ -64,7 +63,7 @@ class MobilkuController extends Website_Controller_Action {
 		$register->setIndex(0);
 		$register->setPublished(1);
 		$register->save();
-		
+		/*die();
 		$emailDocument = '/email/register';
 		$params = array('year' => $th,
                         'price' => $harga,
@@ -86,7 +85,7 @@ class MobilkuController extends Website_Controller_Action {
 		$mail->send();
 		print_r($mail);
 		
-
+		die();*/
 	}
 	
 	public function dataMobileAction(){
