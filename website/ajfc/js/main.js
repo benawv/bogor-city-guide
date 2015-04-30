3220baa3d642efc334ab99e972be4bcbe0f2d106
mrobbin <mrobbi.nugraha@gmail.com>
2015-04-30 18:35:28 +0700

    css ajfc to smartmed

---
+++ website/ajfc/js/main.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+    $('.tab-content').hide();
+    $('.tab-content:first').show();
+    $('.tab-button ul li:first').addClass('active');
+    $('.tab-button ul li a').click(function(){
+        $('.tab-button ul li').removeClass('active');
+        $(this).parent().addClass('active');
+        var currentTab = $(this).attr('href');
+        $('.tab-content').hide();
+        $(currentTab).show();
+        return false;
+    });
+
+    $('.tab_button ul li a:first').addClass('active');
+    $('.tab_button ul li a').click(function(){
+        $('.tab_button ul li a').removeClass('active');
+        $(this).addClass('active');
+        var currentTab = $(this).attr('href');
+        $('.tab-content').hide();
+        $(currentTab).show();
+        return false;
+    });
+
+    $(".menu-level1 > ul > li").hover(function () {
+        var height_search = $('div.search').position().top;
+        if($(window).width() >= 960){
+            var currentMenu = $(this);
+            currentMenu.children(".menu-level2").slideDown({
+                duration:200,
+                start: function(){
+                    $('div.search').addClass('aktif');
+
+                },
+                step: function(){
+                    if (height_search<=40) {
+                        $('div.search').css('top',$(this).height() + 38);
+                    }
+                },
+                complete: function(){
+                    $(".menu-level2").removeClass('active').hide();
+                    $(this).addClass('active');
+                }
+            });
+        }
+        else
+        {
+            $('div.search').removeClass('aktif');
+            $(".menu-level2").removeClass('active');
+            $(this).children(".menu-level2").addClass('active');
+        }
+    });
+    $('.primary').mouseleave(function () {
+        if($(window).width() >= 960)
+        {
+            $(".menu-level2").hide().removeClass('active');
+            $('div.search').removeClass('aktif');
+        }
+        else
+        {
+            $(".menu-level2").removeClass('active');
+            $('div.search').removeClass('aktif');
+        }
+
+    });
+    $("nav.primary .toggle-menu").click(function() {
+        if($(window).width() < 960)
+            $("nav.primary .menu-level1").toggleClass('active');
+    });
+
+});
+
