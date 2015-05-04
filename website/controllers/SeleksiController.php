391d23d0177ba3943cbfcb06f304691332a740a6
mrobbin <mrobbi.nugraha@gmail.com>
2015-05-04 23:18:28 +0700

    expire normal 23.59

--- website/controllers/SeleksiController.php
+++ website/controllers/SeleksiController.php
@@ -14,12 +14,12 @@ class SeleksiController extends Website_Controller_Action {
 				$tgl1 = new Pimcore_Date($today);
 				$tgl2 = $row->getTglExpireKuis();
 				
-				$date2 = strtotime(date('Y-m-d 23:59'));
-				$today2 = new Pimcore_Date($date2);
+				//$date2 = strtotime(date('Y-m-d 23:59'));
+				//$today2 = new Pimcore_Date($date2);
 				//echo $today;
 				//die();
-				//if($tgl1 <= $tgl2)
-				if($tgl1 <= $today2)
+				//if($tgl1 <= $today2)
+				if($tgl1 <= $tgl2)
 				{
 					$this->enableLayout();
 					$this->setLayout('ajfc');
