ca550ac8efca963584faf6d706a0d1967ab2fd72
mrobbin <mrobbi.nugraha@gmail.com>
2015-05-06 16:41:52 +0700

    sitemap ajfc

--- website/views/areas/sitemap/view.php
+++ website/views/areas/sitemap/view.php
@@ -1,12 +1,12 @@
 <?php
 	if(!$this->document instanceof Document_Page) {
-        $this->document = Document::getById(1);
+        $this->document = Document::getById(805);
     }
  
     // get the document which should be used to start in navigation | default home
     $navStartNode = $this->document->getProperty("navigationRoot");
     if(!$navStartNode instanceof Document_Page) {
-        $navStartNode = Document::getById(1);
+        $navStartNode = Document::getById(805);
     }
  
     //this is used as id prefix for the html menu element
