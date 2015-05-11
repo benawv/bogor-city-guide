d8262e4a1ea9418cc7cf6e84835624f4580e86b3
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2015-05-11 13:19:34 +0700

    GA liveajfc

--- website/views/scripts/includes/metadata.php
+++ website/views/scripts/includes/metadata.php
@@ -89,6 +89,18 @@ Remarketing tags may not be associated with personally identifiable information 
 
 <script type="text/javascript">
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-44171460-7']);
+_gaq.push(['_setAllowLinker', true]);
+_gaq.push(['_setAllowHash', false]);
+_gaq.push(['_trackPageview']);
+_gaq.push(['_trackPageLoadTime']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 /* <![CDATA[ */
 
 var google_conversion_id = 972850354;
