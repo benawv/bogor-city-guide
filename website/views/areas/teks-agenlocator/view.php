2eeded96f8e5c4e0df78250cb81323de4ea5e5dc
BastianRDP <bastianrdp@gmail.com>
2015-05-11 16:53:16 +0700

    agenlocator

---
+++ website/views/areas/teks-agenlocator/view.php
@@ -0,0 +1,36 @@
+<div id="<?php echo $this->input('anchor')->text?>" class="full-w bg-white">
+	<h2>
+		<?php if($this->editmode): ?>
+			Icon: <?php echo $this->image('icon', array(
+								'title' 	=> 'Image Size 31x31',
+								'width' 	=> 100,
+								'height' 	=> 100,
+								'thumbnail'	=> 'icon')) ?>
+			<?php echo $this->input('title', ["width" => 250])?>
+		<?php else: ?>
+			<?php if($this->image('icon')->getSrc()):?>
+				<div style="background-image: url('<?php echo $this->image('icon')->getSrc()?>'); height: 31px; width: 31px; float: left; margin-right: 10px; "></div>
+			<?php endif;?>
+		<span class="judul_section"><?php echo $this->input('title')?></span>
+		<?php endif; ?>
+		<div class="btn-group btn-section">
+			<a href="javascript:void(0);" class="fbshare"><i class="fa fa-facebook"></i></a>
+			<a href="javascript:void(0);" class="twshare"><i class="fa fa-twitter"></i></a>
+		</div>
+	</h2>
+	<div class="description width-66">
+		<?php if($this->editmode): ?>
+			<?php echo "anchor name: ".$this->input('anchor', array("width" => 250))?>
+		<?php endif;?>
+		<div class="section-left-40">
+			<?php echo $this->image('image-kiri', array(
+									'title' => 'Image Size 226x200',
+									'width' => 226,
+									'height' => 200
+									/*'thumbnail' => 'product'*/))?>
+		</div>
+		<div class="section-right-60">
+			<?php echo $this->wysiwyg('teks-kanan')?>
+		</div>
+	</div>
+</div>
