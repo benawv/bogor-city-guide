572d0260d9e5f1b22228bfb8a3519e2738350abe
Fitrazh <fitrazh@gmail.com>
2015-05-12 03:41:17 +0700

    pindah tasbih imkepo

---
+++ website/views/areas/inquiry-form/view.php
@@ -0,0 +1,225 @@
+<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/mobilku/bootstrap.min.css"  />
+<link rel="stylesheet" type="text/css" media="screen" id="normalize-css" href="/website/static/css/normalize.css">
+<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/screen.css">
+<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/main.css">
+
+<link rel="stylesheet" type="text/css" media="all" href="/website/static/mobilku/tasbih/tasbih.css" />
+<link rel="stylesheet" type="text/css" media="all" href="/website/static/mobilku/wizard-step.css" />
+
+<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="_assets/css/flexslider.css">-->
+<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="_assets/css/tabs-accordion.css">-->
+<!--<link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/css/product.css">-->
+
+<script src="/website/static/js/masonry.min.js" type="text/javascript"></script>
+
+<style>
+    /* Override AJFC's feature box color */
+    .box-dent--inner{ background: #009a44; }
+    .box-dent::before{ border-top: 28px solid #009a44 !important;}
+    .btn-tasbih{ background: #009a44 !important; }
+    .btn-tasbih:hover, .btn-tasbih:focus{ background: #007a53 !important;  }
+</style>
+<?php if( $this->editmode ): ?>
+<style>
+    .navi{
+        margin-bottom: 50px;
+    }
+    .flex-viewport{
+        margin-bottom: 50px;
+    }
+    .wrapper-special{
+        margin-bottom: 100px;
+    }
+    .navi li{
+        float: none;
+    }
+</style>
+<?php endif; ?>
+
+<link rel="stylesheet" type="text/css" media="all" href="/website/ajfc/css/style.css" />
+
+<script src="/website/static/js/masonry.min.js" type="text/javascript"></script>
+<script src="/website/static/js/jquery.tools.min.js" type="text/javascript"></script>
+
+<?php if( !$this->editmode ): ?>
+<script src="/website/static/js/sticky-jquery.js" type="text/javascript"></script>
+<?php endif; ?>
+
+<div id="newsletter-allianz" class="full-w bg-white nopadding">
+
+    <div class="description">
+
+        <div class="header-calc">
+            <h1>KALKULATOR ALLIANZ TASBIH</h1>
+        </div><!--/ .header-calc -->
+
+        <div class="content-calc">
+            <div class="col-md-12">
+
+                <form class="form-horizontal" role="form">
+
+                    
+
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Nama</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="nama" placeholder="Nama" required onfocusout="this.value=validateNama(this.value)"> 
+                            <label id="notif-nama" style="display:none; color: #f00;">
+                                Mohon maaf Anda belum atau salah memasukkan nama
+                            </label>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+                    
+                   
+                    
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Jenis Kelamin</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
+                            <select class="form-control" name="JenisKelamin" id="JenisKelamin">
+                                <option value="l">Pria</option>
+                                <option value="p">Wanita</option>
+                            </select>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+
+
+                    
+                    
+                                        <div class="form-group">
+                        <div class="col-md-4">
+                            <label>DOB</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="DOB" value="" placeholder="DOB">
+                            <label id="notif-asuransijiwa" style="display:none; color: #f00;">
+                                Mohon maaf inputan yang Anda masukkan belum benar
+                            </label>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+
+<div class="form-group">
+                        <div class="col-md-4">
+                            <label>Email</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Email" required onfocusout="this.value=validateEMAIL(this.value)">
+                            <label id="notifemail" style="display:none; color: #f00;">
+                                Mohon maaf email yang Anda masukkan belum benar
+                            </label>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+                    
+
+
+            
+
+                    <div class="form-group">
+                        <div class="col-md-4">
+                            <label>Kontribusi Berkala/ Premi pertahun</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
+                            <input type="email" class="form-control" id="kontribusi-berkala" placeholder="0" readonly>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+                    
+                     <div class="form-group">
+                        <div class="col-md-4">
+                            <label>No Handphone (Min. 8 digit)</label>
+                        </div><!--/ .col-md-4 -->
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" id="nohp" name="nohp" placeholder="No Handphone" required onfocusout="this.value=validateNumber(this.value)">
+                            <label id="notifNoHP" style="display:none; color: #f00;">
+                                Mohon maaf No HP yang Anda masukkan belum benar
+                            </label>
+                        </div><!--/ .col-md-4 -->
+                    </div><!--/ .form-group -->
+
+                    <div class="form-group">
+                        <div class="col-md-2 col-md-offset-4">
+                            <input type="button" class="btn btn-next btn-fill btn-warning btn-wd btn-sm btn-tasbih" id="Submit" name="Submit" value="SUbmit">
+                        </div><!--/ .col-md-12 -->
+                    </div><!--/ .form-group -->
+
+                </form>
+
+                
+
+            </div><!--/ .col-md-12 -->
+        </div><!--/ .content-calc -->
+    </div><!--/ .description -->
+</div><!--/ #newsletter-allianz -->
+
+<link rel="stylesheet" href="/website/static/mobilku/jquery-ui.css">
+<script src="/website/static/js/jquery-1.9.1.min.js" type="text/javascript"></script>
+<script src="/website/static/mobilku/jquery-ui.js" type="text/javascript"></script>
+
+
+<script src="/website/static/inv/js/accounting.min.js" type="text/javascript"></script>
+
+<script>
+    // document.getElementById('tgl-hitung').value = (new Date()).format("m/dd/yy");
+
+    
+    //Validate Number
+    $('#nohp').bind("input", function(){
+       var re = /^[0-9]*$/; 
+        
+        var value = $('#nohp').val();
+        $(this).val(value);
+        if(!re.test(value)){
+            document.getElementById('notifNoHP').style.display= 'block';
+            $('#nohp').val('');
+        }else{
+            document.getElementById('notifNoHP').style.display= 'none';
+        }
+        
+    });
+    
+    function validateNumber(value){
+        if(value.length <= 8 ){
+            document.getElementById('notifNoHP').style.display= 'block';
+            return null;
+        }else{ 
+            document.getElementById('notifNoHP').style.display= 'none';
+            return value;
+        }
+    };
+
+    //Validate Email
+    function validateEMAIL(surat)
+    {
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+        if(!re.test(surat))
+        {
+            document.getElementById('notifemail').style.display= 'block';
+            return null;
+        }
+        else
+        {
+            document.getElementById('notifemail').style.display= 'none';
+            return surat;
+        }
+    };
+    
+    function validateNama(nama){
+        var re = /^[^\\\/&]*$/;
+        if(!re.test(nama)){
+            document.getElementById('notif-nama').style.display= 'block';
+            return null;
+        }else{
+            document.getElementById('notif-nama').style.display= 'none';
+            return nama;
+        }    
+    };
+    
+           
+
+</script>
+
+<!--
+</div>
+-->
