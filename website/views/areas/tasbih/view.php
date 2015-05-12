817ab2b17ba5a87e084a0c6a3d89cb5baa95d58b
Falmesino Abdul Hamid <falmesino@gmail.com>
2015-05-12 17:21:19 +0700

    added loading box

--- website/views/areas/tasbih/view.php
+++ website/views/areas/tasbih/view.php
@@ -165,6 +165,13 @@
 
                     </div><!--/ .form-box -->
 
+
+                    <div class="form-box text-center">
+                        <i class="fa fa-refresh fa-3x fa-spin"></i>
+                    </div><!--/ .form-box -->
+
+                    <div class="form-box">
+
                     <label><strong>Setoran Tahunan</strong></label>
 
                     <div class="form-group">
@@ -175,6 +182,8 @@
                             <input type="email" class="form-control" id="kontribusi-berkala" placeholder="0" readonly>
                         </div><!--/ .col-md-4 -->
                     </div><!--/ .form-group -->
+
+                    </div><!--/ .form-box -->
 
                     <div class="form-group">
                         <div class="col-md-8 col-md-offset-4">
