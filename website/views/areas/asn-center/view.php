bab6bed2a1e1f05e3ba1735f7454a5667fee9a5d
mrobbin <mrobbi.nugraha@gmail.com>
2015-05-18 10:01:27 +0700

    live - asn download center 3

--- website/views/areas/asn-center/view.php
+++ website/views/areas/asn-center/view.php
@@ -1,8 +1,7 @@
 <?php
-	session_start();
 	if($_SESSION["loginCenter"] == "Yes")
 	{
-		$this->_redirect("/asn/asn-digital-download-center");
+		//$this->_redirect("/asn/asn-digital-download-center");
 	}
 	else{
 		$this->_redirect("/asn/login-asn-center");
