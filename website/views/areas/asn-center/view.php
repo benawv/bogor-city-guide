7f1b2c06a101b5fca0f754b9ae9e21598d210632
mrobbin <mrobbi.nugraha@gmail.com>
2015-05-22 19:14:56 +0700

    live - asn login center jika editmode

--- website/views/areas/asn-center/view.php
+++ website/views/areas/asn-center/view.php
@@ -1,14 +1,16 @@
 <?php
-	$session = new Zend_Session_Namespace('loginasn');
-	
-	if($session->verified === 'true')
-	{
-		//die('session true');
-	}
-	else
-	{
-		//Zend_Controller_Action::redirect()->gotoUrl('/asn/login-asn-center');
-		header('Location: http://www.allianz.co.id/asn/login-asn-center');
-		exit();
+	if(!$this->editmode){
+		$session = new Zend_Session_Namespace('loginasn');
+		
+		if($session->verified === 'true')
+		{
+			//die('session true');
+		}
+		else
+		{
+			//Zend_Controller_Action::redirect()->gotoUrl('/asn/login-asn-center');
+			header('Location: http://www.allianz.co.id/asn/login-asn-center');
+			exit();
+		}
 	}
 ?>
