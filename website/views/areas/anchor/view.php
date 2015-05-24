142833fc157f116948ffd4967a4c0c866a648ece
mrobbin <mrobbi.nugraha@gmail.com>
2015-05-25 00:03:44 +0700

    live - show anchor in mobile

--- website/views/areas/anchor/view.php
+++ website/views/areas/anchor/view.php
@@ -62,6 +62,51 @@
 				</li>
 			<?php } ?>
 		</ul>
+		
+		<div class="customAnchorHide" style="display: none;">
+		<?php 
+			$style = !$this->editmode?'float: left; position: absolute;':'height: 250px';
+		?>
+			<div id="membername" style="width: 580px; position:absolute;">
+				<div class="slide prev"><span class="arrow"></span></div>
+				<div class="super_navi">
+					<ul id="navigate" class="navi" style="<?php echo $style?>">
+						<?php for($i=0;$i<$anchors;$i++) { ?>
+							<?php $active = $i==0?'aktif':''?>
+							<li class="<?php echo $this->input('class_name_'.$i)->text?> <?php echo $active?>">
+								<div class="nav_menu" <?php if($this->editmode) { ?> style="color: #000;" <?php } ?> >
+									<?php echo $this->editmode?'<b>Anchor Name:</b> '. $this->input('class_name_'.$i, array('width' => '75')) : ''?>
+									
+									<?php if($this->editmode) { ?>
+										<b>Icon Inactive:</b> <?php echo $this->image('icon_'.$i, array(
+														'title' 	=> 'Image Size 26x26',
+														'width' 	=> 100,
+														'height' 	=> 100,
+														'thumbnail'	=> 'icon')) ?>
+									<?php } else { ?>
+										<div style="background-image: url('<?php echo $this->image('icon_'.$i)->getSrc()?>'); margin-bottom: 3px; width: 26px; height: 26px; background-repeat: no-repeat;"></div>
+									<?php } ?>
+									
+									<?php if($this->editmode) { ?>
+										<b>Icon Active:</b> <?php echo $this->image('icon_'.$i.'_'.$i, array(
+														'title' 	=> 'Image Size 26x26',
+														'width' 	=> 100,
+														'height' 	=> 100,
+														'thumbnail'	=> 'icon')) ?>
+									<?php } else { ?>
+										<div class="white_image" style="background-image: url('<?php echo $this->image('icon_'.$i.'_'.$i)->getSrc()?>'); margin-bottom: 3px; width: 26px; height: 26px; background-repeat: no-repeat;"></div>
+									<?php } ?>
+									
+									<?php echo $this->editmode?'Title: ':'' ?><?php echo $this->input('anchor_title_'.$i, array("width" => '75'))?>
+								</div>
+							</li>
+						<?php } ?>
+					</ul>
+				</div>
+				<div class="slide next"></div>
+			</div>
+		</div>
+		
 	<?php } else { ?>
 	<?php 
 		$style = !$this->editmode?'float: left; position: absolute;':'height: 250px';
