80fdcd533946e5a66bd2268f14c41117c292e945
Falmesino Abdul Hamid <falmesino@gmail.com>
2015-05-29 11:34:17 +0700

    thankyou page floating menu positioning fix

--- website/views/areas/thankyou-inquiry/view.php
+++ website/views/areas/thankyou-inquiry/view.php
@@ -12,7 +12,7 @@
 
     <div>
 
-        
+
 <div class="pimcore_area_tasbih pimcore_area_content">
 <link rel="stylesheet" type="text/css" media="screen" id="screen-css" href="/website/static/mobilku/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" media="screen" id="normalize-css" href="/website/static/css/normalize.css">
@@ -37,7 +37,7 @@
     .form-box{ border: solid 1px #ddd; padding: 16px 20px; margin-bottom: 32px; }
     .form-control{ border-radius: 0; }
         nav.main-navigation a.nav-item.home::after {
-      content: "\f015"; }        
+      content: "\f015"; }
     nav.main-navigation a.nav-item.chat::after {
       content: "\f003  "; }
     nav.main-navigation a.nav-item {
@@ -67,15 +67,15 @@
   pointer-events: none;
 }
         nav.main-navigation a.nav-item.blueA{
-      background: #003781;}        
-    
+      background: #003781;}
+
     nav.main-navigation a.nav-item.greenChat{
       background: #B20065;}
 
       nav.main-navigation a.nav-item.blueA::before {
         background: #3122F9; }
 </style>
-<header>
+<header style="margin: 48px auto 0;">
         <nav class="main-navigation" style="max-width:1140px; margin:auto;">
             <div class="row">
 
@@ -84,25 +84,25 @@
                         <h4 style="font-size:18px"><small>Beranda</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
-                
+
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
                     <a href="/kalkulator/tasbih" class="nav-item green sitemap">
                         <h4 style="font-size:18px"><small>Ilustrasi</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
-                
+
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
                     <a href="/tasbih/tanya-jawab" class="nav-item orange question">
                         <h4 style="font-size:18px"><small>Pertanyaan Umum</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
-                
+
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
                     <a href="http://agen.imkepo.com" target="_blank" class="nav-item red users">
                         <h4 style="font-size:18px"><small>Cari Agen</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
-                
+
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
                     <a href="/tasbih/inquiry-form" class="nav-item grey chat">
                         <h4 style="font-size:18px"><small>Minta Informasi</small></h4>
@@ -110,10 +110,10 @@
                 </div><!--/ .col-xs-12 -->
             </div>
             <div class="row">
-                
+
             </div>
     </nav>
-    
+
 </header>
 <div id="newsletter-allianz" class="full-w bg-white nopadding">
 
@@ -127,7 +127,7 @@
             <div class="col-md-12">
 
                 <div class="form-box" id="notif-confirm">
-    
+
     <h3  style=" color:black;"><strong>Terima kasih Anda telah mengirimkan permintaan Anda. Anda akan segera dihubungi oleh agen Allianz Star Network atau temukan agen terdekat. </strong></h3>
                    <br />
     <center><input type="button" class="btn btn-next btn-fill btn-warning btn-wd btn-sm btn-tasbih" id="cari-agen" name="next" value="Cari Agen"></center>
@@ -139,7 +139,7 @@
 </div><!--/ #newsletter-allianz -->
 
 <link rel="stylesheet" href="/website/static/mobilku/jquery-ui.css">
-        
+
 
 
 <!--
@@ -167,18 +167,18 @@
             var target = '#modal-' + hash;
             $( target ).modal( 'show' );
         }
-        
+
         $(this).on('click', '.inquiry-send', function(e){
-            
+
             var nama=$('#nama').val();
             var kelamin=$('#JenisKelamin').val();
             var bod=$('#DOB').val();
             var email=$('#email').val();
             var nohp=$('#nohp').val();
             var prov=$('#provinsi_id').val();
-            
+
             //alert(bod);
-            
+
             if( nama == '' || email == '' || nohp == '' || bod == '' ||  nohp.length <= 8 || prov == ''){
                     if( nama == ''  )
                         document.getElementById('notif-nama').style.display= 'block';
@@ -229,15 +229,15 @@
                 $('html, body').animate({scrollTop:$("#"+id[0]).offset().top-90}, 500);
             }
         });
-        
+
         $('#DOB').datepicker({
             changeMonth: true,
             changeYear: true,
             yearRange: "-100:+0"
         });
-        
+
     });
-    
+
     function validateEMAIL(surat)
     {
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
@@ -252,7 +252,7 @@
             return surat;
         }
     };
-    
+
     $('#nohp').bind("input", function(){
        var re = /^[0-9]*$/;
 
@@ -265,22 +265,22 @@
             document.getElementById('notifNoHP').style.display= 'none';
         }
 
-    });   
+    });
     $('#pesan').bind("input", function(){
         var n = $('#pesan').val().length;
         var max = 250;
-        
+
         if(n > max){
-               $('#pesan').attr('readonly','readonly');        
+               $('#pesan').attr('readonly','readonly');
         }
-        
+
         $('#counterString').html(250-n);
-        
+
 //        var CountStr = parseInt($('#counterString').html());
-//        var re = /^[a-zA-Z0-9]*$/; 
+//        var re = /^[a-zA-Z0-9]*$/;
 //        var del = /^[\b]*$/;
 //        var value= $(this).html();
-//        
+//
 //        if(del.value){
 //            alert('test');
 //            document.getElementById('notif-nama').style.disbled= 'false';
@@ -300,9 +300,9 @@
 //        }
 
     });
-    
 
-    
+
+
     function validateNumber(value){
         if(value.length <= 8 ){
             document.getElementById('notifNoHP').style.display= 'block';
@@ -314,5 +314,5 @@
     };
 </script>
 
-	</div>
+    </div>
 
