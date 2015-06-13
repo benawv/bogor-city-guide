ae9d2996edc6fb032d4818b90bf45120964825d5
fitrazh <fitrazh@gmail.com>
2015-06-13 12:40:48 +0700

    tasbih: dari co.id

--- website/views/areas/Tasbih-kedua/view.php
+++ website/views/areas/Tasbih-kedua/view.php
@@ -108,31 +108,31 @@
             <div class="row">
 
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px;margin:auto; !important">
-                    <a href="/tasbih" class="nav-item blueA home">
+                    <a href="/produk/asuransi-syariah/tasbih" class="nav-item blueA home">
                         <h4 style="font-size:18px"><small>Beranda</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
 
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                    <a href="/kalkulator/tasbih" class="nav-item green sitemap">
+                    <a href="/produk/asuransi-syariah/tasbih/info-produk" class="nav-item orange question">
+                        <h4 style="font-size:18px"><small>Informasi produk</small></h4>
+                    </a>
+                </div><!--/ .col-xs-12 -->
+
+                <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
+                    <a href="/produk/asuransi-syariah/tasbih/kalkulator/ilustrasi" class="nav-item green sitemap">
                         <h4 style="font-size:18px"><small>Ilustrasi</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
 
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                    <a href="/tasbih/tanya-jawab" class="nav-item orange question">
-                        <h4 style="font-size:18px"><small>Pertanyaan Umum</small></h4>
-                    </a>
-                </div><!--/ .col-xs-12 -->
-
-                <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                    <a href="http://agen.imkepo.com" target="_blank" class="nav-item users">
+                    <a href="http://agen.allianz.co.id" target="_blank" class="nav-item users">
                         <h4 style="font-size:18px"><small>Cari Agen</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
 
                 <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                    <a href="/tasbih/inquiry-form" class="nav-item grey chat">
+                    <a href="/produk/asuransi-syariah/tasbih/minta-informasi" class="nav-item grey chat">
                         <h4 style="font-size:18px"><small>Minta Informasi</small></h4>
                     </a>
                 </div><!--/ .col-xs-12 -->
@@ -217,7 +217,6 @@
                                 <!--
                               <input type="button" class="kalkulasi btn btn-next btn-fill btn-warning btn-wd btn-sm btn-tasbih" id="Submit" name="next" value="Submit">
                                 -->
-                                
                                 <button type="button" class="kalkulasi btn btn-next btn-fill btn-warning btn-wd btn-sm btn-tasbih" id="Submit" name="next">
                                     <i class="fa fa-envelope-o"></i> Kirim
                                 </button>
@@ -322,7 +321,7 @@
                             foreach ($entries as $key) {
                          ?>
                             <li>
-                                <a href="tasbih/artikel/<?php echo $key->o_key."-".$key->o_id; ?>">
+                                <a href="/produk/asuransi-syariah/tasbih/artikel/<?php echo $key->o_key."-".$key->o_id; ?>">
                                     <?php echo $key->title; ?>
                                 </a>
                                 <div class="news-date"><?php echo date("D, d M Y,",strtotime($key->newsdate)); ?></div>
@@ -421,7 +420,6 @@
         /*
          * jQueryUI DatePicker
          */
-        
 
         $('#Submit').click(function() {
 
@@ -442,7 +440,7 @@
             }else{
 
               $.ajax({
-                  url      : '/kalkulator-tasbih-kedua/',
+                  url      : '/tasbih-kedua/',
                   type     : 'POST',
                   data     : {
                               'nama' : nama,
@@ -455,7 +453,7 @@
                         $('.waiting-calc').hide();
                         $('.result-calc').show();
                         //$('#hasil').val(accounting.formatMoney(data, "Rp ", 0,","));
-                        document.location.href = "/tasbih/thankyou";
+                        document.location.href = "/produk/asuransi-syariah/tasbih/kalkulator/thankyou";
                       }
               });
             }
--- website/views/areas/inquiry-form/view.php
+++ website/views/areas/inquiry-form/view.php
@@ -48,27 +48,27 @@
     <nav class="main-navigation" style="max-width:960px; margin:auto;">
         <div class="row">
             <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px;margin:auto; !important">
-                <a href="/tasbih" class="nav-item blueA home">
+                <a href="/produk/asuransi-syariah/tasbih" class="nav-item blueA home">
                     <h4 style="font-size:18px"><small>Beranda</small></h4>
                 </a>
             </div><!--/ .col-xs-12 -->
             <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                <a href="/kalkulator/tasbih" class="nav-item green sitemap">
-                    <h4 style="font-size:18px"><small>Ilustrasi</small></h4>
-                </a>
-            </div><!--/ .col-xs-12 -->
-            <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                <a href="/tasbih/tanya-jawab" class="nav-item orange question">
+                <a href="/produk/asuransi-syariah/tasbih/info-produk" class="nav-item orange question">
                     <h4 style="font-size:18px"><small>Informasi produk</small></h4>
                 </a>
             </div><!--/ .col-xs-12 -->
             <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                <a href="http://agen.imkepo.com" target="_blank" class="nav-item red users">
+                <a href="/produk/asuransi-syariah/tasbih/kalkulator/ilustrasi" class="nav-item green sitemap">
+                    <h4 style="font-size:18px"><small>Ilustrasi</small></h4>
+                </a>
+            </div><!--/ .col-xs-12 -->
+            <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
+                <a href="http://agen.allianz.co.id" target="_blank" class="nav-item red users">
                     <h4 style="font-size:18px"><small>Cari Agen</small></h4>
                 </a>
             </div><!--/ .col-xs-12 -->
             <div class="col-xs-12 col-md-2" style="min-width:20%; min-height:50px; margin:auto; !important">
-                <a href="/tasbih/inquiry-form" class="nav-item grey chat">
+                <a href="/produk/asuransi-syariah/tasbih/minta-informasi" class="nav-item grey chat">
                     <h4 style="font-size:18px"><small>Minta Informasi</small></h4>
                 </a>
             </div><!--/ .col-xs-12 -->
@@ -192,7 +192,7 @@
                             <label><strong>Nama Anda?</strong></label>
                         </div><!--/ .col-md-4 -->
                         <div class="col-md-4">
-                            <input type="name" class="form-control" id="nama" placeholder="Nama" required onfocusout="this.value=validateNama(this.value)">
+                            <input type="name" class="form-control" id="nama" placeholder="Nama" required />
                             <label id="notif-nama" style="display:none; color: #f00;">
                                 Mohon maaf Anda belum atau salah memasukkan nama
                             </label>
@@ -213,9 +213,6 @@
                                 <div class="col-xs-6">
                                     <label class="radio-inline"><input type="Radio" style="display: block;" name="jenisKelamin" value="p" style="display:block">Wanita</label>
                                 </div>
-                                <label id="notifJK" style="display:none; color: #f00;">
-                                Mohon maaf Anda belum memilih jenis kelamin
-                            </label>
                             </div><!--/ .row -->
                         </div><!--/ .col-md-4 -->
                     </div><!--/ .form-group -->
@@ -283,20 +280,19 @@
                             <label><strong>Pesan</strong></label>
                         </div><!--/ .col-md-4 -->
                         <div class="col-md-4">
-                            
+                            <label id="notifPesan" style="display:none; color: #f00;">
+                                Mohon maaf, Anda harus mengisi pesan.
+                            </label>
                             <textarea cols="35" rows="10" class="form-control" placeholder="Tuliskan Pesan Anda disini Maksimal 250 Kata" id="pesan" ></textarea>
                             <label id="notifemail" style="display:block; font-size:10px; color: #f00;">
                                 Sisa karakter yang Anda tulis : <span id="counterString">250</span>  Kata
-                            </label>
-                            <label id="notifPesan" style="display:none; color: #f00;">
-                                Mohon maaf inputan yang Anda masukkan melebihi batas jumlah karakter
                             </label>
                         </div><!--/ .col-md-4 -->
                     </div><!--/ .form-group -->
 
                     <div class="form-group">
                         <div class="col-md-2 col-md-offset-4">
-                            <a href="/tasbih/thankyou-inquiry">
+                            <a href="/tasbih/thankyou-minta-informasi">
                                 <input type="button" class="btn btn-next btn-fill btn-warning btn-wd btn-sm btn-tasbih inquiry-send" id="Submit" name="Submit" value="Kirim">
                             </a>
                         </div><!--/ .col-md-12 -->
@@ -322,7 +318,7 @@
                             foreach ($entries as $key) {
                          ?>
                             <li>
-                                <a href="tasbih/artikel/<?php echo $key->o_key."-".$key->o_id; ?>">
+                                <a href="/produk/asuransi-syariah/tasbih/artikel/<?php echo $key->o_key."-".$key->o_id; ?>">
                                     <?php echo $key->title; ?>
                                 </a>
                                 <div class="news-date"><?php echo date("D, d M Y,",strtotime($key->newsdate)); ?></div>
@@ -361,10 +357,6 @@
             $( target ).modal( 'show' );
         }
 
-        $('input[name=jenisKelamin]').change(function(){
-            document.getElementById('notifJK').style.display= 'none';
-        });
-        
         $(this).on('click', '.inquiry-send', function(e){
 
             var nama=$('#nama').val();
@@ -375,18 +367,9 @@
             var prov=$('#provinsi_id').val();
             var pesan=$('#pesan').val();
 
-            var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-            if(re.test(email))
-            {
-                document.getElementById('notifemail').style.display= 'none';
-            }
-            else
-            {
-                document.getElementById('notifemail').style.display= 'block';
-                email = "";
-            }
-            
-            if( nama == '' || email == '' || nohp == '' || bod == '' ||  nohp.length <= 8 || prov == '' || pesan == '' || kelamin == null){
+            //alert(bod);
+
+            if( nama == '' || email == '' || nohp == '' || bod == '' ||  nohp.length <= 8 || prov == '' || pesan == ''){
                     if( nama == ''  )
                         document.getElementById('notif-nama').style.display= 'block';
                     if( email == '' )
@@ -399,12 +382,9 @@
                         document.getElementById('notifNoHP').style.display='block';
                     if( pesan == '' )
                         document.getElementById('notifPesan').style.display= 'block';
-                    if( kelamin == null)
-                        document.getElementById('notifJK').style.display= 'block';
                     alert("Mohon Periksa Inputan Anda");
             }else{
             e.preventDefault();
-            alert("else");
             $.ajax({
                  "url" : "/inquriy-tasbih/",
                  "type" : "POST",
@@ -417,7 +397,7 @@
                           "&pesan="+pesan,
                  "success" : function(response){
                     if (response == "success") {
-                        window.location = "/tasbih/thankyou-inquiry";
+                        window.location = "/produk/asuransi-syariah/tasbih/thankyou-minta-informasi";
                     }
                  }
              });
@@ -477,14 +457,14 @@
     function validateEMAIL(surat)
     {
         var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-        if(re.test(surat))
+        if(!re.test(surat))
         {
-            document.getElementById('notifemail').style.display= 'none';
+            document.getElementById('notifemail').style.display= 'block';
             return surat;
         }
         else
         {
-            document.getElementById('notifemail').style.display= 'block';
+            document.getElementById('notifemail').style.display= 'none';
             return surat;
         }
     };
@@ -507,9 +487,7 @@
         var max = 250;
 
         if(n > max){
-               document.getElementById('notifPesan').style.display= 'block';
-        }else{
-            document.getElementById('notifPesan').style.display= 'none';
+               $('#pesan').attr('readonly','readonly');
         }
 
         $('#counterString').html(250-n);
@@ -539,16 +517,7 @@
 
     });
 
-function validateNama(nama){
-        var re = /^[a-zA-Z ]*$/;
-        if(!re.test(nama)){
-            document.getElementById('notif-nama').style.display= 'block';
-            return null;
-        }else{
-            document.getElementById('notif-nama').style.display= 'none';
-            return nama;
-        }
-    };
+
 
     function validateNumber(value){
         if(value.length <= 8 ){
