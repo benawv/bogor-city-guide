5e222ac0c662ffc8ed3fdf3101b877487c7d1cad
irwan adriansyah <adriansyah.irwan@gmail.com>
2015-07-09 12:02:33 +0700

    kalukator simamet

--- website/views/areas/smartmed/view.php
+++ website/views/areas/smartmed/view.php
@@ -767,7 +767,7 @@
         for(var i = 0; i < rowCount; i++){
             total += parseInt($('table.table tbody').children()[0].children[15].innerHTML);
             totalwithoutuwl += parseInt($('table.table tbody').children()[0].children[16].innerHTML);
-            console.log("total"+total);
+            console.log("total"+parseInt($('table.table tbody').children()[0].children[15].innerHTML));
         }
         $('table.table tfoot tr:first').children()[0].innerHTML = total;
         if(total>=250000 && total<1000000) stamp = 3000;
