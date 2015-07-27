d54de258beafe00169f87993b1fa6e3ce7dc5652
stmardiah <stmardiah3@gmail.com>
2015-07-27 18:34:30 +0700

    profil maker - tambah select option di page pertama profil maker untuk menentukan berapa jumlah template yang akan di tampilkan

--- website/views/areas/profil-maker/view.php
+++ website/views/areas/profil-maker/view.php
@@ -1,8 +1,33 @@
-<?php 
-setcookie("gettamplate","value",time()+$int);
-?>
+<div id="slideshow" class="clearfix">
+<?php if($this->editmode) { ?>
+    <div class="alert alert-info" style="height: 75px">
+        <div class="col-xs-6">
+            How many images you want to show?
 
+            <?php
+                
+                $select= [];
+                for($i=1; $i<30; $i++) {
+                    $select[] = [$i, $i];
+                }
+              ?>
+            <?php echo $this->select("jumlah",[
+                "store" => $select,
+                "reload" => true
+            ]); ?>
 
+        </div>
+    </div>
+
+    <style type="text/css">
+        .gallery .item {
+            min-height: 377px;
+        }
+        .place-bg-gallery{
+            height: 265px !important;
+        }
+    </style>
+<?php } ?>
 <link rel="stylesheet" href="/website/static/css/bootstrap-grid-only.css">
 <link rel="stylesheet" href="/website/static/css/profil-maker.css">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
@@ -14,10 +39,12 @@ setcookie("gettamplate","value",time()+$int);
                 <h2>Allianz Profile Maker</h2>
             </div><!--/ .col-xs-12 -->
         </div><!--/ .row -->
+        <?php $val = (int) $this->select("jumlah")->getData();
+        ?>
 
-        <?php for( $i = 0; $i < 2; $i++ ): ?>
+        <?php for( $i = 0; $i < $val; $i++ ): ?>
 
-        <div class="row">
+        <!--<div class="row">-->
 
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
@@ -30,7 +57,7 @@ setcookie("gettamplate","value",time()+$int);
                 </div><!--/ .profile-maker-box -->
             </div><!--/ .col-xs-12 -->
 
-            <div class="col-xs-12 col-md-4">
+            <!--<div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
                     <h4>Page Cover Landscaped Notepad</h4>
                     <img src="http://placehold.it/640x320" alt="Template Image" class="img-responsive">
@@ -38,10 +65,10 @@ setcookie("gettamplate","value",time()+$int);
                         <a href="/profil-maker/page2">
                             <input type="button" id="template2" onclick="getId(this.id)" value="Pilih" class="btn btn-primary"/>
                         </a>
-                        <!--<a href="/profil-maker-create" class="btn btn-primary">Pilih</a>-->
-                    </div><!--/ .pm-buttons -->
-                </div><!--/ .profile-maker-box -->
-            </div><!--/ .col-xs-12 -->
+                        <!--<a href="/profil-maker-create" class="btn btn-primary">Pilih</a>
+                    </div><!--/ .pm-buttons 
+                </div><!--/ .profile-maker-box 
+            </div><!--/ .col-xs-12 
 
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
@@ -51,12 +78,12 @@ setcookie("gettamplate","value",time()+$int);
                         <a href="/profil-maker/page2">
                             <input type="button" id="template3" onclick="getId(this.id)" value="Pilih" class="btn btn-primary"/>
                         </a>
-                        <!--<a href="/profil-maker/2" class="btn btn-primary">Pilih</a>-->
-                    </div><!--/ .pm-buttons -->
-                </div><!--/ .profile-maker-box -->
+                        <!--<a href="/profil-maker/2" class="btn btn-primary">Pilih</a>
+                    </div><!--/ .pm-buttons 
+                </div><!--/ .profile-maker-box 
             </div><!--/ .col-xs-12 -->
 
-        </div><!--/ .row -->
+        <!--/</div> .row -->
 
         <script type="text/javascript">
             function setCookie(cname, cvalue) {
