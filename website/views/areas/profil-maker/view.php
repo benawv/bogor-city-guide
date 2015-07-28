de5c56c6d9a1ba63544ebc0238186573bbc2d386
stmardiah <stmardiah3@gmail.com>
2015-07-28 10:42:43 +0700

    profil maker - update page 1

--- website/views/areas/profil-maker/view.php
+++ website/views/areas/profil-maker/view.php
@@ -1,6 +1,6 @@
 <div id="slideshow" class="clearfix">
-<?php if($this->editmode) { ?>
-    <div class="alert alert-info" style="height: 75px">
+<?php //if($this->editmode) { ?>
+    <!--<div class="alert alert-info" style="height: 75px">
         <div class="col-xs-6">
             How many images you want to show?
 
@@ -26,8 +26,8 @@
         .place-bg-gallery{
             height: 265px !important;
         }
-    </style>
-<?php } ?>
+    </style>-->
+<?php //} ?>
 <link rel="stylesheet" href="/website/static/css/bootstrap-grid-only.css">
 <link rel="stylesheet" href="/website/static/css/profil-maker.css">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
@@ -39,13 +39,12 @@
                 <h2>Allianz Profile Maker</h2>
             </div><!--/ .col-xs-12 -->
         </div><!--/ .row -->
-        <?php $val = (int) $this->select("jumlah")->getData();
+        <?php //$val = (int) $this->select("jumlah")->getData();
         ?>
 
-        <?php for( $i = 0; $i < $val; $i++ ): ?>
+        <?php for( $i = 0; $i < 2; $i++ ): ?>
 
-        <!--<div class="row">-->
-
+        <div class="row">
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
                     <h4>Page Cover Landscaped Notepad</h4>
@@ -57,7 +56,7 @@
                 </div><!--/ .profile-maker-box -->
             </div><!--/ .col-xs-12 -->
 
-            <!--<div class="col-xs-12 col-md-4">
+            <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
                     <h4>Page Cover Landscaped Notepad</h4>
                     <img src="http://placehold.it/640x320" alt="Template Image" class="img-responsive">
@@ -65,10 +64,10 @@
                         <a href="/profil-maker/page2">
                             <input type="button" id="template2" onclick="getId(this.id)" value="Pilih" class="btn btn-primary"/>
                         </a>
-                        <!--<a href="/profil-maker-create" class="btn btn-primary">Pilih</a>
-                    </div><!--/ .pm-buttons 
-                </div><!--/ .profile-maker-box 
-            </div><!--/ .col-xs-12 
+                        <!--<a href="/profil-maker-create" class="btn btn-primary">Pilih</a>-->
+                    </div><!--/ .pm-buttons -->
+                </div><!--/ .profile-maker-box -->
+            </div><!--/ .col-xs-12 -->
 
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
@@ -78,13 +77,16 @@
                         <a href="/profil-maker/page2">
                             <input type="button" id="template3" onclick="getId(this.id)" value="Pilih" class="btn btn-primary"/>
                         </a>
-                        <!--<a href="/profil-maker/2" class="btn btn-primary">Pilih</a>
-                    </div><!--/ .pm-buttons 
-                </div><!--/ .profile-maker-box 
+                        <!--<a href="/profil-maker/2" class="btn btn-primary">Pilih</a>-->
+                    </div><!--/ .pm-buttons -->
+                </div><!--/ .profile-maker-box -->
             </div><!--/ .col-xs-12 -->
 
-        <!--/</div> .row -->
+        </div> <!--/.row -->
 
+
+        <?php endfor; ?>
+        
         <script type="text/javascript">
             function setCookie(cname, cvalue) {
                 document.cookie = cname + "=" + cvalue + "; ";
@@ -111,8 +113,6 @@
            }
 
         </script>
-
-        <?php endfor; ?>
 
     </div><!--/ .container -->
 </section><!--/ .profile-maker -->
