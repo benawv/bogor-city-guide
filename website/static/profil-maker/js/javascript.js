86fdf202f291b67ee637f3d33b02f54dfdf5a116
akbarfachri <akbar.fachriansyah@gmail.com>
2015-07-28 11:29:39 +0700

    download image active

--- website/static/profil-maker/js/javascript.js
+++ website/static/profil-maker/js/javascript.js
@@ -197,7 +197,7 @@ $(document).ready(function()
 	                        //$('#imaged').html(canvas);
 	                            var dataURL = canvas.toDataURL("image/png");
 
-	                           	$('#imaged').append('<img src="'+dataURL+'" />');
+	                           	//$('#imaged').append('<img src="'+dataURL+'" />');
 	                            //$('#imaged').html('Generating..');
 	                            // $.post('image.php',{image: dataURL},function(data){
 	                            //     $('#imaged').html(data);
@@ -205,13 +205,13 @@ $(document).ready(function()
 	                            // });
 
 	                			// Random filename after download
-	                			//var filename = new Array(5).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36);})
+	                			var filename = new Array(5).join().replace(/(.|$)/g, function(){return ((Math.random()*36)|0).toString(36);})
 
-	        						// var link = document.createElement('a');
-									// link.href = dataURL;
-									// link.download = filename+".png";
-									// document.body.appendChild(link);
-									// link.click();
+	        						var link = document.createElement('a');
+									link.href = dataURL;
+									link.download = filename+".png";
+									document.body.appendChild(link);
+									link.click();
 
 	       						//var a = $("<a>")
 								//.attr("href", dataURL)
