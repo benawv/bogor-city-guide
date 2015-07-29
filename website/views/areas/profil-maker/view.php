52a4fa9fb50673e0dec3db8bd76c5eaa9ea0a8e1
stmardiah <stmardiah3@gmail.com>
2015-07-29 11:06:08 +0700

    profil maker - update page 1, src foto & urutan template

--- website/views/areas/profil-maker/view.php
+++ website/views/areas/profil-maker/view.php
@@ -14,50 +14,11 @@
 
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
-                    <h4 id="News Feed Post Template">News Feed Post Template</h4>
-                    <img src="\website\static\images\profile-maker\Agent Facebook News Feed Post Template.png" alt="Template Image" class="img-responsive">
+                    <h4>Page Cover Landscaped Notepad</h4>
+                    <img src="/website/static/images/profile-maker/Allianz Agent Page Cover Landscaped Notepad.png" alt="Template Image" class="img-responsive">
                     <div class="pm-buttons">
                         <a href="/profil-maker/page2">
                             <input type="button" id="template1" value="Pilih" class="btn btn-primary template"/>
-                        </a>
-                        <!--<a href="/profil-maker-create" class="btn btn-primary">Pilih</a>-->
-                    </div><!--/ .pm-buttons -->
-                </div><!--/ .profile-maker-box -->
-            </div><!--/ .col-xs-12 -->
-
-            <div class="col-xs-12 col-md-4">
-                <div class="profile-maker--box">
-                    <h4>Profile Picture</h4>
-                    <img src="/website\static\images\profile-maker\/Allianz Agent Facebook Profile Picture.png" alt="Template Image" class="img-responsive">
-                    <div class="pm-buttons">
-                        <a href="/profil-maker/page2">
-                            <input type="button" id="template2" value="Pilih" class="btn btn-primary template"/>
-                        </a>
-                        <!--<a href="/profil-maker/2" class="btn btn-primary">Pilih</a>-->
-                    </div><!--/ .pm-buttons -->
-                </div><!--/ .profile-maker-box -->
-            </div><!--/ .col-xs-12 -->
-
-            <div class="col-xs-12 col-md-4">
-                <div class="profile-maker--box">
-                    <h4>Timeline Post Template</h4>
-                    <img src="\website\static\images\profile-maker\Allianz Facebook Timeline Post Template.png" alt="Template Image" class="img-responsive">
-                    <div class="pm-buttons">
-                        <a href="/profil-maker/page2"> 
-                            <input type="button" id="template3" value="Pilih" class="btn btn-primary template"/>
-                        </a>
-                        <!--a href="/profil-maker/2" id="template1" class="btn btn-primary">Pilih</a>-->
-                    </div><!--/ .pm-buttons -->
-                </div><!--/ .profile-maker-box -->
-            </div><!--/ .col-xs-12 -->
-
-            <div class="col-xs-12 col-md-4">
-                <div class="profile-maker--box">
-                    <h4>Page Cover Landscaped Notepad</h4>
-                    <img src="\website\static\images\profile-maker\Allianz Agent Page Cover Landscaped Notepad.png" alt="Template Image" class="img-responsive">
-                    <div class="pm-buttons">
-                        <a href="/profil-maker/page2">
-                            <input type="button" id="template4" value="Pilih" class="btn btn-primary template"/>
                         </a>
                         <!--a href="/profil-maker/2" id="template1" class="btn btn-primary">Pilih</a>-->
                     </div><!--/ .pm-buttons -->
@@ -67,10 +28,10 @@
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
                     <h4>Page Cover Portrait Notepad</h4>
-                    <img src="\website\static\images\profile-maker\Allianz Agent Page Cover Portrait Notepad.png" alt="Template Image" class="img-responsive">
+                    <img src="/website/static/images/profile-maker/Allianz Agent Page Cover Portrait Notepad.png" alt="Template Image" class="img-responsive">
                     <div class="pm-buttons">
                         <a href="/profil-maker/page2">
-                            <input type="button" id="template5" value="Pilih" class="btn btn-primary template"/>
+                            <input type="button" id="template2" value="Pilih" class="btn btn-primary template"/>
                         </a>
                         <!--a href="/profil-maker/2" id="template1" class="btn btn-primary">Pilih</a>-->
                     </div><!--/ .pm-buttons -->
@@ -80,9 +41,48 @@
             <div class="col-xs-12 col-md-4">
                 <div class="profile-maker--box">
                     <h4>Page Cover Square Notepad</h4>
-                    <img src="\website\static\images\profile-maker\Allianz Agent Page Cover Square Notepad.png" alt="Template Image" class="img-responsive">
+                    <img src="/website/static/images/profile-maker/Allianz Agent Page Cover Square Notepad.png" alt="Template Image" class="img-responsive">
                     <div class="pm-buttons">
                         <a href="/profil-maker/page2">
+                            <input type="button" id="template3" value="Pilih" class="btn btn-primary template"/>
+                        </a>
+                        <!--a href="/profil-maker/2" id="template1" class="btn btn-primary">Pilih</a>-->
+                    </div><!--/ .pm-buttons -->
+                </div><!--/ .profile-maker-box -->
+            </div><!--/ .col-xs-12 -->
+
+            <div class="col-xs-12 col-md-4">
+                <div class="profile-maker--box">
+                    <h4 id="News Feed Post Template">News Feed Post Template</h4>
+                    <img src="/website/static/images/profile-maker/Agent Facebook News Feed Post Template.png" alt="Template Image" class="img-responsive">
+                    <div class="pm-buttons">
+                        <a href="/profil-maker/page2">
+                            <input type="button" id="template4" value="Pilih" class="btn btn-primary template"/>
+                        </a>
+                        <!--<a href="/profil-maker-create" class="btn btn-primary">Pilih</a>-->
+                    </div><!--/ .pm-buttons -->
+                </div><!--/ .profile-maker-box -->
+            </div><!--/ .col-xs-12 -->
+
+            <div class="col-xs-12 col-md-4">
+                <div class="profile-maker--box">
+                    <h4>Profile Picture</h4>
+                    <img src="/website/static/images/profile-maker/Allianz Agent Facebook Profile Picture.png" alt="Template Image" class="img-responsive">
+                    <div class="pm-buttons">
+                        <a href="/profil-maker/page2">
+                            <input type="button" id="template5" value="Pilih" class="btn btn-primary template"/>
+                        </a>
+                        <!--<a href="/profil-maker/2" class="btn btn-primary">Pilih</a>-->
+                    </div><!--/ .pm-buttons -->
+                </div><!--/ .profile-maker-box -->
+            </div><!--/ .col-xs-12 -->
+
+            <div class="col-xs-12 col-md-4">
+                <div class="profile-maker--box">
+                    <h4>Timeline Post Template</h4>
+                    <img src="/website/static/images/profile-maker/Allianz Facebook Timeline Post Template.png" alt="Template Image" class="img-responsive">
+                    <div class="pm-buttons">
+                        <a href="/profil-maker/page2"> 
                             <input type="button" id="template6" value="Pilih" class="btn btn-primary template"/>
                         </a>
                         <!--a href="/profil-maker/2" id="template1" class="btn btn-primary">Pilih</a>-->
