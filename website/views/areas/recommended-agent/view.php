2a2f733b1aaf48b94e1d2f0090dd5193791016d2
mrobbin <mrobbi.nugraha@gmail.com>
2015-08-12 10:53:45 +0700

    agen-locator : box top agen

--- website/views/areas/recommended-agent/view.php
+++ website/views/areas/recommended-agent/view.php
@@ -11,7 +11,7 @@
             <?php
                 // prepare the store
                 $selectStore = [];
-                for($i=1; $i<30; $i++) {
+                for($i=1; $i<=10; $i++) {
                     $selectStore[] = [$i, $i];
                 }
             ?>
@@ -23,7 +23,7 @@
         <?php } ?>
         <div class="row profile-maker--title">
             <div class="col-xs-12">
-                <h2>Rekomendasi Agen</h2>
+                <h2><?php echo $this->input("heading");?></h2>
             </div><!--/ .col-xs-12 -->
         </div><!--/ .row -->
         
@@ -33,40 +33,65 @@
         else
             $agent = 1;
         for( $i = 1; $i <= $agent; $i++ ): 
-            if($i%5==0){
-                echo "<div class='row'>";
-                $temp = "</div>";
-            }else{
+            //if($i%5==0){
+            //    echo "<div class='row'>";
+            //    $temp = "</div>";
+            //}else{
                 $link = "myLink-link".$i;
                 $class = "thumbnail thumbnail-popup id".$i;
                 $target = "#modalpdf".$i;
                 $gambar = "gambar-tahukah-anda".$i;
                 $class_gambar = "thumb1 clickPage".$i;
                 // echo '
-                // <div class="col-xs-12 col-md-20">
-                //     <div class="profile-maker--box">
-                //         <h4>Your Name '.$this->link("myLink-link1",array("class" => "thumbnail thumbnail-popup id1",
-                //                                          "data-target" => "#modalpdf1",
-                //                                          "data-toggle" => "modal")).'</h4>
-                //         <img src="/website/static/images/profil.jpg" alt="Template Image" class="img-responsive">
-                        
-                //     </div><!--/ .profile-maker-box -->
-                // </div><!--/ .col-xs-12 -->
-                // ';
-                echo '
-                <div class="col-xs-12 col-md-20">
-                    <div class="profile-maker--box">
-                        <h4>'.$this->link($link,array("class" => $class,
-                                                         "data-target" => $target,
-                                                         "data-toggle" => "modal")).'</h4>
-                        '.$this->image($gambar, array("class" => $class_gambar,"title" => "Image Size 166x202","width" => 166,"height" => 202)).'
-                        
-                    </div><!--/ .profile-maker-box -->
-                </div><!--/ .col-xs-12 -->
-                ';
-                echo $temp;
-                $temp = "";
-            }
+			?>
+                 <div class="col-xs-12 col-md-20">
+                     <div class="profile-maker--box">
+                         <h4><?php
+								//if($this->editmode) {
+								//	echo $this->link($link,array("class" => $class));
+								//}else{
+								//	$id = $this->link($link,array("class" => $class))->getHref();
+								//	$data = Object_AgentLocatorData::getById($id);
+								//	echo "<pre>";
+								//	print_r($data->getO_id());
+								//	die();
+								//	if($data->getKantor()[0]->o_id != ""){
+							?>
+										<!--<a href="<?php //echo $_SERVER['HTTP_REFERER']."/detail-agent/".$data->getKantor()[0]->o_id."-".$data->getO_id();?>" target="_blank"><?php echo $this->link($link,array("class" => $class))->getText();?></a>-->
+							<?php
+								//	}
+								//}
+							?>
+							<?php echo $this->renderlet("artikel".$i, array(
+								"controller" => "agent",
+								"action" => "renderlet",
+								"editmode" => $this->editmode,
+								"title" => "Drag objek agen yang diinginkan."
+								));
+							?>
+						 </h4>
+                         <!--<img src="/website/static/images/profil.jpg" alt="Template Image" class="img-responsive">-->
+						<?php
+							echo $this->image($gambar, array("class" => $class_gambar,"title" => "Image Size 166x202","width" => 166,"height" => 202));
+						?>
+                     </div><!--/ .profile-maker-box -->
+                 </div><!--/ .col-xs-12 -->
+            <?php
+			    // ';
+                //echo '
+                //<div class="col-xs-12 col-md-20">
+                //    <div class="profile-maker--box">
+                //        <h4>'.$this->link($link,array("class" => $class,
+                //                                         "data-target" => $target,
+                //                                         "data-toggle" => "modal")).'</h4>
+                //        '.$this->image($gambar, array("class" => $class_gambar,"title" => "Image Size 166x202","width" => 166,"height" => 202)).'
+                //        
+                //    </div><!--/ .profile-maker-box -->
+                //</div><!--/ .col-xs-12 -->
+                //';
+                //echo $temp;
+                //$temp = "";
+            //}
 
         endfor; ?>
 
