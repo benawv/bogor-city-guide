b179aa2be977608039da048f97e66987708a8d75
Falmesino Abdul Hamid <falmesino@gmail.com>
2015-08-18 17:01:16 +0700

    add white background and title with an icon

--- website/views/areas/recommended-agent/view.php
+++ website/views/areas/recommended-agent/view.php
@@ -2,10 +2,20 @@
 <link rel="stylesheet" href="/website/static/css/profil-maker.css">
 <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
-	<section class="profile-maker">
+<style>
+    .full-w .thumbnail.thumbnail-popup
+    {
+        background-image: none;
+        color: black;
+    }
+</style>
+
+<section class="profile-maker full-w bg-white">
     <div class="container">
+
         <?php if($this->editmode) { ?>
-        <div class="col-xs-6">
+        <div class="row">
+            <div class="col-xs-6">
             How many Agent you want to show?
 
             <?php
@@ -19,20 +29,39 @@
                 "store" => $selectStore,
                 "reload" => true
             ]); ?>
-        </div>
+            </div><!--/ .col-xs-6 -->
+        </div><!--/ .row -->
         <?php } ?>
+
+        <h2>
+            <?php if($this->editmode): ?>
+                Icon: <?php echo $this->image('icon_', array(
+                                    'title' 	=> 'Image Size 31x31',
+                                    'width' 	=> 100,
+                                    'height' 	=> 100,
+                                    'thumbnail'	=> 'icon')) ?>
+            <?php else: ?>
+                <?php if($this->image('icon_')->getSrc()):?>
+                    <div style="background-image: url('<?php echo $this->image('icon_')->getSrc()?>'); height: 31px; width: 31px; float: left; margin-right: 10px; "></div>
+                <?php endif;?>
+
+            <?php endif; ?>
+            <?php echo $this->input("heading");?>
+        </h2>
+
+        <!--
         <div class="row profile-maker--title">
             <div class="col-xs-12">
                 <h2><?php echo $this->input("heading");?></h2>
-            </div><!--/ .col-xs-12 -->
+            </div>
         </div><!--/ .row -->
-        
-        <?php 
+
+        <?php
         if(!$this->select("agent")->isEmpty())
             $agent = (int) $this->select("agent")->getData();
         else
             $agent = 1;
-        for( $i = 1; $i <= $agent; $i++ ): 
+        for( $i = 1; $i <= $agent; $i++ ):
             //if($i%5==0){
             //    echo "<div class='row'>";
             //    $temp = "</div>";
@@ -43,54 +72,54 @@
                 $gambar = "gambar-tahukah-anda".$i;
                 $class_gambar = "thumb1 clickPage".$i;
                 // echo '
-			?>
+            ?>
                  <div class="col-xs-12 col-md-20">
                      <div class="profile-maker--box">
                          <h4><?php
-								if($this->editmode) {
-									echo $this->link($link,array("class" => $class));
-								}else{
-									$id_agen = $this->link($link,array("class" => $class))->getHref();
-									if($id_agen != "" && $id_agen != " " && $id_agen != "#"){
-									$kondisi = array("condition" => "kodeAgent = ".$id_agen);;
-									$getID = Object_AgentLocatorData::getList($kondisi);
-									foreach($getID as $row)
-									{
-										$id = $row->getO_id();
-									}
-									
-									$data = Object_AgentLocatorData::getById($id);
-									//echo "<pre>";//print_r($data->getO_id());//die();
-									//if($data->getKantor()[0]->o_id != ""){
-							?>
-										<a href="<?php echo '/detail-agent/'.$data->getKantor()[0]->o_id.'-'.$data->getO_id();?>" target="_blank"><?php echo $this->link($link)->getText();?></a>
-							<?php
-									}
-									else{
-							?>
-										<a href="#"><?php echo $this->link($link)->getText();?></a>
-							<?php
-									//}
-									}
-								}
-							?>
-							<?php
-								/*echo $this->renderlet("artikel".$i, array(
-								"controller" => "agent",
-								"action" => "renderlet",
-								"editmode" => $this->editmode,
-								"title" => "Drag objek agen yang diinginkan."
-								));*/
-							?>
-						 </h4>
+                                if($this->editmode) {
+                                    echo $this->link($link,array("class" => $class));
+                                }else{
+                                    $id_agen = $this->link($link,array("class" => $class))->getHref();
+                                    if($id_agen != "" && $id_agen != " " && $id_agen != "#"){
+                                    $kondisi = array("condition" => "kodeAgent = ".$id_agen);;
+                                    $getID = Object_AgentLocatorData::getList($kondisi);
+                                    foreach($getID as $row)
+                                    {
+                                        $id = $row->getO_id();
+                                    }
+
+                                    $data = Object_AgentLocatorData::getById($id);
+                                    //echo "<pre>";//print_r($data->getO_id());//die();
+                                    //if($data->getKantor()[0]->o_id != ""){
+                            ?>
+                                        <a href="<?php echo '/detail-agent/'.$data->getKantor()[0]->o_id.'-'.$data->getO_id();?>" target="_blank"><?php echo $this->link($link)->getText();?></a>
+                            <?php
+                                    }
+                                    else{
+                            ?>
+                                        <a href="#"><?php echo $this->link($link)->getText();?></a>
+                            <?php
+                                    //}
+                                    }
+                                }
+                            ?>
+                            <?php
+                                /*echo $this->renderlet("artikel".$i, array(
+                                "controller" => "agent",
+                                "action" => "renderlet",
+                                "editmode" => $this->editmode,
+                                "title" => "Drag objek agen yang diinginkan."
+                                ));*/
+                            ?>
+                         </h4>
                          <!--<img src="/website/static/images/profil.jpg" alt="Template Image" class="img-responsive">-->
-						<?php
-							echo $this->image($gambar, array("class" => $class_gambar,"title" => "Image Size 166x202","width" => 166,"height" => 202));
-						?>
+                        <?php
+                            echo $this->image($gambar, array("class" => $class_gambar,"title" => "Image Size 166x202","width" => 166,"height" => 202));
+                        ?>
                      </div><!--/ .profile-maker-box -->
                  </div><!--/ .col-xs-12 -->
             <?php
-			    // ';
+                // ';
                 //echo '
                 //<div class="col-xs-12 col-md-20">
                 //    <div class="profile-maker--box">
@@ -98,7 +127,7 @@
                 //                                         "data-target" => $target,
                 //                                         "data-toggle" => "modal")).'</h4>
                 //        '.$this->image($gambar, array("class" => $class_gambar,"title" => "Image Size 166x202","width" => 166,"height" => 202)).'
-                //        
+                //
                 //    </div><!--/ .profile-maker-box -->
                 //</div><!--/ .col-xs-12 -->
                 //';
