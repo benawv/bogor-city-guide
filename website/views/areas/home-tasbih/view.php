b0af356a4a897c3aa279bf9b133f81c2c6046b63
mrobbin <mrobbi.nugraha@gmail.com>
2015-08-21 13:51:03 +0700

    live : tasbih - hide tombol ilustrasi di tasbih.

--- website/views/areas/home-tasbih/view.php
+++ website/views/areas/home-tasbih/view.php
@@ -275,6 +275,12 @@
                         <h4 style="font-size:18px"><small>Ilustrasi</small></h4>
                     </a>
                 </div>--><!--/ .col-xs-12 -->
+                
+                <div class="col-xs-12 col-md-3" style="min-width:20%; min-height:50px; margin:auto; !important">
+                    <a href="/produk/asuransi-syariah/tasbih/info-produk#anchor5" class="nav-item green sitemap">
+                        <h4 style="font-size:18px"><small>Proyeksi Dana Naik Haji</small></h4>
+                    </a>
+                </div><!--/ .col-xs-12 -->
 
                 <div class="col-xs-12 col-md-3" style="min-width:20%; min-height:50px; margin:auto; !important">
                     <a href="http://agen.allianz.co.id" target="_blank" class="nav-item red users">
