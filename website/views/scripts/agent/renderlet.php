4272c9ed390690df6b5f3870a194e1427064fe66
mrobbin <mrobbi.nugraha@gmail.com>
2015-08-29 22:11:56 +0700

    live : beta to live tasbih

---
+++ website/views/scripts/agent/renderlet.php
@@ -0,0 +1,2 @@
+<a href="<?php echo "/detail-agent/".$this->data->getKantor()[0]->o_id."-".$this->data->getO_id();?>" target="_blank"><?php echo $this->data->getNamaAgent();?></a>
+<?php //echo $this->url(array($this->data->getKantor()[0]->o_id,$this->data->getO_id(),"detail-agent"));?>
