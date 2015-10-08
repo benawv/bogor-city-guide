eb48d54e22211e678c5ce748e260b1099f46328f
Bastian Ramadhan Dwi Putra <bastianrdp@gmail.com>
2015-10-08 17:12:26 +0700

    Investment : Change words of Button in Investasi Calculator Page

--- website/views/scripts/investment/calculatorinvestiframe.php
+++ website/views/scripts/investment/calculatorinvestiframe.php
@@ -240,7 +240,7 @@ var sliderCurrentValue = $( "#slider3" ).slider( "option", "value" );
            
            <div class="calc-machine">
                <div class="calc-submit bottom">
-                    <input class="orange-btn bg_fund" type="button" value="Hitung Biaya Kebutuhan Saya di Masa Depan">
+                    <input class="orange-btn bg_fund" type="button" value="Hitung Kebutuhan Masa Depan">
                 </div>   
            </div>   
                
