612807b609d722c48972d4d5b876e36f1515aaee
mrobbin <mrobbi.nugraha@gmail.com>
2015-10-13 17:12:01 +0700

    live - edit widget anchor

--- website/views/areas/anchor/view.php
+++ website/views/areas/anchor/view.php
@@ -26,10 +26,92 @@
 ?>
 
 <?php if(!$this->editmode): ?>
+	<?php if($anchors < 6) { ?>
+	<?php 
+		$style = $this->editmode?'height: 250px':'';
+	?>
+		<ul id="membername" class="navi" style="<?php echo $style?>">
+			<?php for($i=0;$i<$anchors;$i++) { ?>
+				<?php $active = $i==0?'aktif':''?>
+				<li class="<?php echo $this->input('class_name_'.$i)->text?> <?php echo $active?>">
+					<div class="nav_menu" <?php if($this->editmode) { ?> style="color: #000;" <?php } ?> >
+						<?php echo $this->editmode?'<b>Anchor Name:</b> '. $this->input('class_name_'.$i, array('width' => '75')) : ''?>
+						
+						<?php if($this->editmode) { ?>
+							<b>Icon Inactive:</b> <?php echo $this->image('icon_'.$i, array(
+											'title' 	=> 'Image Size 26x26',
+											'width' 	=> 100,
+											'height' 	=> 100,
+											'thumbnail'	=> 'icon')) ?>
+						<?php } else { ?>
+							<div style="background-image: url('<?php echo $this->image('icon_'.$i)->getSrc()?>'); margin-bottom: 3px; width: 26px; height: 26px; background-repeat: no-repeat;"></div>
+						<?php } ?>
+						
+						<?php if($this->editmode) { ?>
+							<b>Icon Active:</b> <?php echo $this->image('icon_'.$i.'_'.$i, array(
+											'title' 	=> 'Image Size 26x26',
+											'width' 	=> 100,
+											'height' 	=> 100,
+											'thumbnail'	=> 'icon')) ?>
+						<?php } else { ?>
+							<div class="white_image" style="background-image: url('<?php echo $this->image('icon_'.$i.'_'.$i)->getSrc()?>'); margin-bottom: 3px; width: 26px; height: 26px; background-repeat: no-repeat;"></div>
+						<?php } ?>
+						
+						<span class="cus_anchor_<?php echo $i;?>"><?php echo $this->editmode?'Title: ':'' ?><?php echo $this->input('anchor_title_'.$i, array("width" => '75'))?></span>
+					</div>
+				</li>
+			<?php } ?>
+		</ul>
+		
+		<div class="customAnchorHide" style="display: none;">
+		<?php 
+			$style = !$this->editmode?'float: left; position: absolute;':'height: 250px';
+		?>
+			<div id="membername" style="width: 580px; position:absolute;">
+				<div class="slide prev"><span class="arrow"></span></div>
+				<div class="super_navi">
+					<ul id="navigate" class="navi" style="<?php echo $style?>">
+						<?php for($i=0;$i<$anchors;$i++) { ?>
+							<?php $active = $i==0?'aktif':''?>
+							<li class="<?php echo $this->input('class_name_'.$i)->text?> <?php echo $active?>">
+								<div class="nav_menu" <?php if($this->editmode) { ?> style="color: #000;" <?php } ?> >
+									<?php echo $this->editmode?'<b>Anchor Name:</b> '. $this->input('class_name_'.$i, array('width' => '75')) : ''?>
+									
+									<?php if($this->editmode) { ?>
+										<b>Icon Inactive:</b> <?php echo $this->image('icon_'.$i, array(
+														'title' 	=> 'Image Size 26x26',
+														'width' 	=> 100,
+														'height' 	=> 100,
+														'thumbnail'	=> 'icon')) ?>
+									<?php } else { ?>
+										<div style="background-image: url('<?php echo $this->image('icon_'.$i)->getSrc()?>'); margin-bottom: 3px; width: 26px; height: 26px; background-repeat: no-repeat;"></div>
+									<?php } ?>
+									
+									<?php if($this->editmode) { ?>
+										<b>Icon Active:</b> <?php echo $this->image('icon_'.$i.'_'.$i, array(
+														'title' 	=> 'Image Size 26x26',
+														'width' 	=> 100,
+														'height' 	=> 100,
+														'thumbnail'	=> 'icon')) ?>
+									<?php } else { ?>
+										<div class="white_image" style="background-image: url('<?php echo $this->image('icon_'.$i.'_'.$i)->getSrc()?>'); margin-bottom: 3px; width: 26px; height: 26px; background-repeat: no-repeat;"></div>
+									<?php } ?>
+									
+									<?php echo $this->editmode?'Title: ':'' ?><?php echo $this->input('anchor_title_'.$i, array("width" => '75'))?>
+								</div>
+							</li>
+						<?php } ?>
+					</ul>
+				</div>
+				<div class="slide next"></div>
+			</div>
+		</div>
+		
+	<?php } else { ?>
 	<?php 
 		$style = !$this->editmode?'float: left; position: absolute;':'height: 250px';
 	?>
-		<div id="membername" style="width: 507px; position:absolute;">
+		<div id="membername" style="width: 580px; position:absolute;">
 			<div class="slide prev"><span class="arrow"></span></div>
 			<div class="super_navi">
 				<ul id="navigate" class="navi" style="<?php echo $style?>">
@@ -67,6 +149,7 @@
 			</div>
 			<div class="slide next"></div>
 		</div>
+	<?php } ?>
 <?php else : ?>
 	<?php
 		if($anchors <= 6) {
@@ -137,7 +220,7 @@
 	var w = 0;
 	
 	$('.navi li').each(function(index){
-		width = $(this).width();
+		width = ($(this).width())+10;
 		w += width;
 	});
 	
@@ -156,7 +239,6 @@
 			$(this).find('.xicon').addClass('down')
 		}
 	});*/
-    
 	
 	$('.next').on('click',function(){
 		var me_left = $('.super_navi').scrollLeft();
@@ -169,15 +251,5 @@
 		$('.super_navi').scrollLeft(me_left - 100);
 		//console.log($('.super_navi').position().left);
 	});
-    
-    $(document).ready(function(){
-        var widthNavi = $('#navigate').width();
-        if(widthNavi < 500){
-            $('.slide').hide();
-        }else{
-            $('.slide').show();
-            $('.super_navi').width(470);
-        }
-    });
 	/* silde nav*/
 </script>
